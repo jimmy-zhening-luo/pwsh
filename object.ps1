c0884df076a52bbb24af7fcca50434c8cf1eb7d8
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-16 21:56:36 -0700

    more num

--- object.ps1
+++ object.ps1
@@ -1,4 +1,6 @@
 New-Alias count Measure-Object
+New-Alias num Measure-Object
+New-Alias n Measure-Object
 
 New-Alias keyof Get-Member
 New-Alias k Get-Member
@@ -7,5 +9,6 @@ New-Alias z Sort-Object
 
 New-Alias pick Select-Object
 New-Alias of Select-Object
+New-Alias to Select-Object
 
 New-Alias format Format-Table
