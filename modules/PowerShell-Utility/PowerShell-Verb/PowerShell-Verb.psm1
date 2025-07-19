888863dd0c449c42a1ba86fa82d99e0ce187ac48
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-18 17:57:37 -0700

    add wildcard matching and docs for verb

--- modules/PowerShell-Utility/PowerShell-Verb/PowerShell-Verb.psm1
+++ modules/PowerShell-Utility/PowerShell-Verb/PowerShell-Verb.psm1
@@ -1,3 +1,24 @@
+<#
+.SYNOPSIS
+Gets a list of approved PowerShell verbs.
+
+.DESCRIPTION
+The `Get-VerbPowerShell` function gets verbs that are approved for use in PowerShell commands.
+
+It invokes `Get-Verb`, sorts the returned verbs alphabetically, and returns only the `Verb` field as a `String` array.
+
+.LINK
+http://learn.microsoft.com/powershell/module/microsoft.powershell.utility/get-verb
+.LINK
+Get-Verb
+#>
 function Get-VerbPowerShell {
-  (Get-Verb | Sort-Object -Property Verb | Select-Object Verb).Verb
+  [OutputType([string])]
+  param (
+    [string]$Verb = "*"
+  )
+
+  $VerbMatch = $Verb.Contains('*') ? $Verb : ($Verb.Length -lt 3 ? "$Verb*" : "*$Verb*")
+
+  (Get-Verb -Verb $VerbMatch @args | Sort-Object -Property Verb | Select-Object Verb).Verb
 }
