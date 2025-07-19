765b6199f11871977e3aab3c58ef74545a699022
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-19 03:29:05 -0700

    add documentation to Edit-File.psm1, use full name of code

--- Modules/Windows-Shell/Edit-File/Edit-File.psm1
+++ Modules/Windows-Shell/Edit-File/Edit-File.psm1
@@ -1,15 +1,25 @@
 New-Alias -Name edit -Value Edit-File
 New-Alias -Name i -Value Edit-File
+<#
+.SYNOPSIS
+Edit a file in Visual Studio Code.
+
+.DESCRIPTION
+This function is an alias for the Visual Studio Code command line interface, `code.cmd`.
+
+.LINK
+https://code.visualstudio.com/docs/configure/command-line
+#>
 function Edit-File {
   param([string]$Path)
 
   if ($Path) {
     if (Test-Path $Path) {
-      code $Path @args
+      code.cmd $Path @args
     }
     else {
       if ($Path.StartsWith("-")) {
-        code . $Path @args
+        code.cmd . $Path @args
       }
       else {
         throw "File '$Path' does not exist."
@@ -17,6 +27,6 @@ function Edit-File {
     }
   }
   else {
-    code .
+    code.cmd .
   }
 }
