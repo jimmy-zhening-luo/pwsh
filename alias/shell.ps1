cc9ae8cdeb5a4087d240727c5742ebe9fcbac06b
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-18 20:51:47 -0700

    del

--- alias/shell.ps1
+++ alias/shell.ps1
@@ -1,5 +1,6 @@
 Set-Alias clear Clear-Container # default: Clear-Host
 Set-Alias rd Remove-Folder # default: Remove-Item
+Set-Alias del Remove-Folder # default: Remove-Item
 
 New-Alias c Set-Location
 New-Alias l Get-ChildItem
@@ -7,4 +8,3 @@ New-Alias split Split-Path
 
 New-Alias touch New-Item
 New-Alias hash Get-FileHash
-
