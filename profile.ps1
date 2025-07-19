5a5d9df5510455ef0059707a884bab3323524b7e
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-18 17:32:37 -0700

    new aliases for profile again

--- profile.ps1
+++ profile.ps1
@@ -1,14 +1,14 @@
-New-Alias p Edit-Profile
+New-Alias op Edit-Profile
 function Edit-Profile {
   Edit-File $PROFILE_REPO
 }
 
-New-Alias pi Initialize-Profile
+New-Alias oi Initialize-Profile
 function Initialize-Profile {
   Edit-File $PROFILE.CurrentUserAllHosts
 }
 
-New-Alias pp Sync-Profile
+New-Alias os Sync-Profile
 function Sync-Profile {
   Get-Repository -Path $PROFILE_REPO
 }
