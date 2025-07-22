558718231f8ab46421ce631c79e4b57a2a320108
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-21 20:50:20 -0700

    too lazy to learn precedence

--- Modules/Browser-Client/Open-Url/Open-Url.psm1
+++ Modules/Browser-Client/Open-Url/Open-Url.psm1
@@ -18,7 +18,7 @@ function Open-Url {
     [Uri]$Uri
   )
 
-  $Target = $PSCmdlet.ParameterSetName -eq "Uri" ? $Uri : Test-Path $Path ? Resolve-Path $Path : $Path
+  $Target = $PSCmdlet.ParameterSetName -eq "Uri" ? $Uri : (Test-Path $Path ? Resolve-Path $Path : $Path)
 
   Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" $Target
 }
