150041b9abdf6c60463df04219a2a48e6604563f
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-25 14:52:42 -0700

    Get-YouTube.psm1

--- Modules/Get-YouTube/Get-YouTube.psm1
+++ Modules/Get-YouTube/Get-YouTube.psm1
@@ -2,5 +2,5 @@ New-Alias yt yt-dlp
 
 New-Alias yta Get-YouTubeAudio
 function Get-YouTubeAudio {
-  yt-dlp --extract-audio --format "140/bestaudio,bestaudio" @args
+  yt-dlp --format "bestaudio" --extract-audio --audio-format "mp3" --audio-quality 0 --audio-quality 0 --postprocessor-args "-ar 44100" @args
 }
