0cf51271660f89ad72c37c2d78598687b704a73f
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-25 18:21:01 -0700

    toobz

--- Modules/Get-YouTube/Get-YouTube.psd1
+++ Modules/Get-YouTube/Get-YouTube.psd1
@@ -3,6 +3,7 @@
   ModuleVersion     = "1.0.0.0"
   GUID              = "073e1e0b-76ac-46ca-9626-44ab34ae7d50"
   FunctionsToExport = @(
+    "Get-YouTube"
     "Get-YouTubeAudio"
   )
   AliasesToExport   = @(
--- Modules/Get-YouTube/Get-YouTube.psm1
+++ Modules/Get-YouTube/Get-YouTube.psm1
@@ -1,6 +1,26 @@
-New-Alias yt yt-dlp
+New-Alias yt Get-YouTube
+function Get-YouTube {
+  param(
+    [System.String]$Video
+  )
+
+  if (-not $Video) {
+    Write-Error "No video specified."
+    return
+  }
+
+  $VideoUrl = (Test-Url $Video) ? (
+    ($Video.StartsWith('http://') -or $Video.StartsWith('https://')) ? $Video : "https://$Video"
+  ) : "https://www.youtube.com/watch?v=$Video"
+
+  yt-dlp @args -- $VideoUrl
+}
 
 New-Alias yta Get-YouTubeAudio
 function Get-YouTubeAudio {
-  yt-dlp --format "bestaudio" --extract-audio --audio-format "mp3" --audio-quality 0 --audio-quality 0 --postprocessor-args "-ar 44100" @args
+  param(
+    [System.String]$Video
+  )
+
+  Get-YouTube -Video $Video --format "bestaudio" --extract-audio --audio-format "mp3" --audio-quality 0 --audio-quality 0 --postprocessor-args "-ar 44100" @args
 }
