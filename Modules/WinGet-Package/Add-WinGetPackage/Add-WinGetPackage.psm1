01ad42c4143a27d5ba2f78012065d38ae4ccc05c
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-08-17 11:15:20 -0700

    Enhance Add-WinGetPackage function to upgrade packages when no arguments are provided

--- Modules/WinGet-Package/Add-WinGetPackage/Add-WinGetPackage.psm1
+++ Modules/WinGet-Package/Add-WinGetPackage/Add-WinGetPackage.psm1
@@ -3,10 +3,15 @@ New-Alias wga Add-WinGetPackage
 .SYNOPSIS
 Use WinGet to install a new package or upgrade an existing package.
 .DESCRIPTION
-This function is an alias for `winget install`.
+This function is an alias for `winget install`, unless no arguments are provided, in which case it calls `winget upgrade` with no arguments (lists apps with upgrades available).
 .LINK
 https://learn.microsoft.com/en-us/windows/package-manager/winget/install
 #>
 function Add-WinGetPackage {
-  winget install @args
+  if ($args.Length -eq 0) {
+    winget upgrade
+  }
+  else {
+    winget install @args
+  }
 }
