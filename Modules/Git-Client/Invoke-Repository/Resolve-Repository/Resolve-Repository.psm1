7141943fea53d8bfad0fdb7895b349d895dd5ff1
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-08-21 18:35:06 -0700

    Fix path type check in Resolve-Repository function for better accuracy

--- Modules/Git-Client/Invoke-Repository/Resolve-Repository/Resolve-Repository.psm1
+++ Modules/Git-Client/Invoke-Repository/Resolve-Repository/Resolve-Repository.psm1
@@ -1,13 +1,13 @@
 function Resolve-Repository {
   param([System.String]$Path)
 
-  if (Test-Path (Join-Path $Path ".git")) {
+  if (Test-Path (Join-Path $Path ".git") -PathType Container) {
     Resolve-Path $Path
   }
   else {
     $CodeSubpath = Join-Path $CODE ($Path -replace "^\.[\/\\]+", "")
 
-    if (Test-Path (Join-Path $CodeSubpath ".git")) {
+    if (Test-Path (Join-Path $CodeSubpath ".git") -PathType Container) {
       Resolve-Path $CodeSubpath
     }
     else {
