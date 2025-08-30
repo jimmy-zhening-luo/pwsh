98dec92e51ccfcc1e4e4150f81c7b979ddc7e601
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-08-30 16:15:15 -0700

    Refactor alias definitions: introduce ReadOnly and ReadOnlyAll hashtables for improved clarity

--- overrides.ps1
+++ overrides.ps1
@@ -1,7 +1,18 @@
 Set-Alias clear Clear-Line # default: Clear-Host
 Set-Alias rd Remove-Folder # default: Remove-Item
 
-New-Alias -Option ReadOnly clip Set-Clipboard # default: clip.exe
-New-Alias -Option ReadOnly verb Get-VerbPowerShell # default: implicit Get-Verb
-New-Alias -Option AllScope, ReadOnly run Invoke-CommandPrompt # conflict: nvm/run.cmd
-New-Alias -Option ReadOnly wg winget # conflict: WireGuard/wg.exe
+$ReadOnly = @{
+  Option = 'ReadOnly'
+}
+
+$ReadOnlyAll = @{
+  Option = @(
+    'ReadOnly'
+    'AllScope'
+  )
+}
+
+New-Alias clip Set-Clipboard @ReadOnly # default: clip.exe
+New-Alias verb Get-VerbPowerShell @ReadOnly # default: implicit Get-Verb
+New-Alias run Invoke-CommandPrompt @ReadOnlyAll # conflict: nvm/run.cmd
+New-Alias wg winget @ReadOnly # conflict: WireGuard/wg.exe
