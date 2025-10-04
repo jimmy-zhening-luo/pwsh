6f666a636d34f0f5a49305bb3f33744d0fa990e9
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-04 12:20:01 -0700

    open history using pwsh

--- Modules/PowerShell-Utility/PowerShell-History/PowerShell-History.psm1
+++ Modules/PowerShell-Utility/PowerShell-History/PowerShell-History.psm1
@@ -1,4 +1,4 @@
 New-Alias oc Edit-History
 function Edit-History {
-  Edit-File (Get-PSReadLineOption).HistorySavePath @args
+  Edit-File (Get-PSReadLineOption).HistorySavePath --profile PowerShell @args
 }
