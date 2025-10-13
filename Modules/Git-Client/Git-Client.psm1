03f077ca906a047ade34a3a24b12046b359afda3
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-13 14:03:37 -0700

    move git verb class after instantiation

--- Modules/Git-Client/Git-Client.psm1
+++ Modules/Git-Client/Git-Client.psm1
@@ -1,3 +1,35 @@
+class GitVerb : System.Management.Automation.IValidateSetValuesGenerator {
+  [System.String[]] GetValidValues() {
+    return [System.String[]] (Import-PowerShellDataFile (Join-Path $PSScriptRoot "Git-Verb.psd1" -Resolve)).GIT_VERB
+  }
+}
+
+$ExportableTypes = @(
+  [GitVerb]
+)
+$TypeAcceleratorsClass = [PSObject].Assembly.GetType(
+  'System.Management.Automation.TypeAccelerators'
+)
+$ExistingTypeAccelerators = $TypeAcceleratorsClass::Get
+foreach ($Type in $ExportableTypes) {
+  if ($Type.FullName -in $ExistingTypeAccelerators.Keys) {
+    throw [System.Management.Automation.ErrorRecord]::new(
+      [System.InvalidOperationException]::new("Unable to register type accelerator '$($Type.FullName)' - Accelerator already exists."),
+      'TypeAcceleratorAlreadyExists',
+      [System.Management.Automation.ErrorCategory]::InvalidOperation,
+      $Type.FullName
+    )
+  }
+}
+foreach ($Type in $ExportableTypes) {
+  $TypeAcceleratorsClass::Add($Type.FullName, $Type)
+}
+$MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
+  foreach ($Type in $ExportableTypes) {
+    $TypeAcceleratorsClass::Remove($Type.FullName)
+  }
+}.GetNewClosure()
+
 New-Alias gitc Invoke-Repository
 New-Alias gg Invoke-Repository
 function Invoke-Repository {
@@ -104,35 +136,3 @@ function Resolve-Repository {
     }
   }
 }
-
-class GitVerb : System.Management.Automation.IValidateSetValuesGenerator {
-  [System.String[]] GetValidValues() {
-    return [System.String[]] (Import-PowerShellDataFile (Join-Path $PSScriptRoot "Git-Verb.psd1" -Resolve)).GIT_VERB
-  }
-}
-
-$ExportableTypes = @(
-  [GitVerb]
-)
-$TypeAcceleratorsClass = [PSObject].Assembly.GetType(
-  'System.Management.Automation.TypeAccelerators'
-)
-$ExistingTypeAccelerators = $TypeAcceleratorsClass::Get
-foreach ($Type in $ExportableTypes) {
-  if ($Type.FullName -in $ExistingTypeAccelerators.Keys) {
-    throw [System.Management.Automation.ErrorRecord]::new(
-      [System.InvalidOperationException]::new("Unable to register type accelerator '$($Type.FullName)' - Accelerator already exists."),
-      'TypeAcceleratorAlreadyExists',
-      [System.Management.Automation.ErrorCategory]::InvalidOperation,
-      $Type.FullName
-    )
-  }
-}
-foreach ($Type in $ExportableTypes) {
-  $TypeAcceleratorsClass::Add($Type.FullName, $Type)
-}
-$MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
-  foreach ($Type in $ExportableTypes) {
-    $TypeAcceleratorsClass::Remove($Type.FullName)
-  }
-}.GetNewClosure()
