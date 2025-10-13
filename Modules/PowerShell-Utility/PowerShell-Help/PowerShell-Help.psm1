8610897b22e067d62eb59ace033387e3b93817d6
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-13 11:38:15 -0700

    Refactor Get-HelpOnline function to streamline error handling and improve clarity

--- Modules/PowerShell-Utility/PowerShell-Help/PowerShell-Help.psm1
+++ Modules/PowerShell-Utility/PowerShell-Help/PowerShell-Help.psm1
@@ -13,52 +13,44 @@ function Get-HelpOnline {
     [System.String[]]$Parameter
   )
 
-  $Articles = @()
-
   if ($Name) {
-    $ErrorStop = @{ ErrorAction = "Stop" }
-
-    if ($HELP.Contains($Name)) {
-      $HELP[$Name] |
-        ForEach-Object {
-          $Articles += $_
-          Open-Url $_
-        }
+    function Show-Help {
+      Get-Help -Name $Name -ErrorAction Stop @args
     }
-    else {
-      try {
-        $Articles += (
-          (
-            Get-Help $Name @ErrorStop
-          ).relatedLinks.navigationLink.Uri |
-            Where-Object { $_ -ne "" } |
-            ForEach-Object { $_ -replace "\?.*$", "" }
-        )
-        Get-Help $Name -Online -ErrorAction Stop | Out-Null
-      }
-      catch {
-        $NameLower = $Name.ToLowerInvariant()
-        $about_Name = $NameLower.StartsWith("about_") ? $Name : $NameLower.StartsWith("about") ? ("about_" + $Name.Substring(5)) : $Name.StartsWith("_") ? "about$Name" : "about_$Name"
-        $AboutArticle = "https://learn.microsoft.com/powershell/module/microsoft.powershell.core/about/$about_Name"
 
-        if (Test-Url $AboutArticle) {
-          $Articles += $AboutArticle
-          Open-Url $AboutArticle
-        }
-        elseif (-not $AboutArticle.EndsWith("s") -and (Test-Url ($AboutArticle + "s"))) {
-          $Articles += $AboutArticle + "s"
-          Open-Url ($AboutArticle + "s")
-        }
+    $Articles = @()
+
+    try {
+      $Articles += (
+        (Show-Help).relatedLinks.navigationLink.Uri |
+          Where-Object { $_ -ne "" } |
+          ForEach-Object { $_ -replace "\?.*$", "" }
+      )
+      Show-Help -Online | Out-Null
+    }
+    catch {
+      $NameLower = $Name.ToLowerInvariant()
+      $about_Name = $NameLower.StartsWith("about_") ? $Name : $NameLower.StartsWith("about") ? ("about_" + $Name.Substring(5)) : $Name.StartsWith("_") ? "about$Name" : "about_$Name"
+      $AboutArticle = "https://learn.microsoft.com/powershell/module/microsoft.powershell.core/about/$about_Name"
+
+      if (Test-Url $AboutArticle) {
+        $Articles += $AboutArticle
+        Open-Url $AboutArticle
+      }
+      elseif (-not $AboutArticle.EndsWith("s") -and (Test-Url ($AboutArticle + "s"))) {
+        $Articles += $AboutArticle + "s"
+        Open-Url ($AboutArticle + "s")
       }
     }
+
 
     if ($Parameter) {
       try {
-        Get-Help $Name -Parameter $Parameter @ErrorStop
+        Show-Help -Parameter $Parameter
       }
       catch {
         try {
-          Get-Help $Name @ErrorStop
+          Show-Help
           Write-Warning "No offline help found for parameters '$Parameter' in topic '$Name'."
         }
         catch {
@@ -68,26 +60,25 @@ function Get-HelpOnline {
     }
     else {
       try {
-        Get-Help $Name @ErrorStop
+        Show-Help
       }
       catch {
         throw "No offline help found for topic '$Name'."
       }
     }
-  }
-  else {
-    Get-Help Get-Help -Online
-    Get-Help Get-Help
-    Write-Warning "No help topic specified, showing Get-Help by default."
-  }
 
-  if ($Local:Articles.Count -gt 0) {
-    "`r`nOnline Help:"
-    $Articles |
-      ForEach-Object { $_ -replace "^https?:\/\/", "http://" } |
-      ForEach-Object { $_ -replace "^learn\.microsoft\.com\/en-us\/", "^learn.microsoft.com/" }
+    if ($Local:Articles.Count -gt 0) {
+      "`r`nDOCS"
+      $Articles |
+        ForEach-Object { $_ -replace "^https?:\/\/", "http://" } |
+        ForEach-Object { $_ -replace "^learn\.microsoft\.com\/en-us\/", "^learn.microsoft.com/" }
+    }
+    else {
+      Write-Warning "No online help found."
+    }
   }
   else {
-    Write-Warning "No online help found."
+    Get-Help -Name Get-Help -Online
+    Get-Help -Name Get-Help
   }
 }
