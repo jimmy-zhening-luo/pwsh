c9a82d9adb9bbd4dce556412c2bda670c1a47934
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-04 13:16:06 -0800

    Fix formatting in Compare-Package function description

--- Modules/Node-PackageManager/Compare-Package/Compare-Package.psm1
+++ Modules/Node-PackageManager/Compare-Package/Compare-Package.psm1
@@ -3,7 +3,7 @@ New-Alias npo Compare-Package
 .SYNOPSIS
 Use Node Package Manager (npm) to check for outdated packages.
 .DESCRIPTION
-This function is an alias for `npm outdated` `[--prefix=$Path]`.
+This function is an alias for `npm outdated [--prefix $Path]`.
 .LINK
 https://docs.npmjs.com/cli/commands/npm-outdated
 #>
