e0250be1a242ad4b18f3df4dfb308748cc7e17b4
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-08 15:28:41 -0800

    update format-count to have return type and handle reversed args

--- Modules/Quick-Tool/Convert-String/Convert-String.psm1
+++ Modules/Quick-Tool/Convert-String/Convert-String.psm1
@@ -1,19 +1,40 @@
 New-Alias plural Format-Count
 function Format-Count {
+  [OutputType([void], [string], [string[]])]
   param(
     [Parameter(Mandatory)]
     [string]$Noun,
-    [Parameter(
-      Mandatory,
-      ValueFromRemainingArguments
-    )]
-    [int[]]$Count
+    [Parameter(ValueFromRemainingArguments)]
+    [string[]]$Count
   )
 
-  $Nouns = $Noun.Contains("/") ? $Noun.Split("/", 2) : @($Noun, "$($Noun)s")
-  $Singular = $Nouns[0]
-  $Plural = $Nouns[1]
+  if ($Noun -as [int]) {
+    if ($Count -and $Count.Count -gt 0) {
+      $Noun, $Count = $Count[-1], (, $Noun + $Count[0..($Count.Count - 2)])
+    }
+    else {
+      $Count = , $Noun
+      $Noun = "item"
+    }
+  }
 
-  $Count |
-    % { "$_ $((($_ -eq 1) -or ($_ -eq -1)) ? $Singular : $Plural)" }
+  if ($Noun -as [int]) {
+    throw "Noun parameter must be a string representing a noun."
+  }
+
+  $Nouns = $Noun.Contains("/") ? $Noun.Split("/", 2) : @(
+    $Noun,
+    "$($Noun)s"
+  )
+
+  if ($Count) {
+    $Count = $Count |
+      ? { $_ -as [int] } |
+      % { [int]$_ }
+
+    if ($Count) {
+      $Count |
+        % { "$_ " + $Nouns[($_ -ne -1) -and ($_ -ne 1)] }
+    }
+  }
 }
