e5d8224824d9b7b8a9db5c960a4da00764c350f6
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-07 16:31:10 -0800

    alias for man to Get-HelpOnline

--- Overrides/Alias.ps1
+++ Overrides/Alias.ps1
@@ -1,5 +1,6 @@
 Set-Alias clear Clear-Line # default: Clear-Host
 Set-Alias rd Remove-Directory # default: Remove-Item
+Set-Alias man Get-HelpOnline # default: Get-Help
 
 $ReadOnly = @{
   Option = 'ReadOnly'
