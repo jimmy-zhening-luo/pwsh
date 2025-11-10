f1941a4b11c97940c6105c51469e9dd158f217a3
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-10 05:00:08 -0800

    Remove ArgumentCompleter.ps1 file and its classes

--- index.ps1
+++ index.ps1
@@ -4,7 +4,6 @@ try {
   $PSDefaultParameterValues = Import-PowerShellDataFile -Path "$PSScriptRoot\Overrides\Param.psd1" -ErrorAction Stop
 
   (. $PSScriptRoot\Overrides\Alias.ps1)
-  (. $PSScriptRoot\Classes\ArgumentCompleter.ps1)
 }
 catch {
   throw "Failed to initialize PowerShell profile.`n$($_.Exception.Message)"
