56e27fd5fe770abb61a9aa1566ad5a69a50766e8
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-09 18:02:54 -0800

    remove outer try catch overhead

--- index.ps1
+++ index.ps1
@@ -1,9 +1,3 @@
 $Env:PSModulePath += ";$PSScriptRoot\Modules"
-
-try {
-  $PSDefaultParameterValues = Import-PowerShellDataFile "$PSScriptRoot\Overrides\Param.psd1" -ErrorAction Stop
-  . $PSScriptRoot\Overrides\Alias.ps1
-}
-catch {
-  throw "Failed to initialize PowerShell profile: $($_.Exception.Message)"
-}
+$PSDefaultParameterValues = Import-PowerShellDataFile "$PSScriptRoot\Overrides\Param.psd1" -ErrorAction Stop
+[void](. $PSScriptRoot\Overrides\Alias.ps1)
