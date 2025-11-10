c5b9be3b6f3b6518636e738cf61bea1f4ab5198e
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-10 04:21:07 -0800

    pwsh

--- index.ps1
+++ index.ps1
@@ -2,10 +2,9 @@ $Env:PSModulePath += ";$PSScriptRoot\Modules"
 
 try {
   $PSDefaultParameterValues = Import-PowerShellDataFile -Path "$PSScriptRoot\Overrides\Param.psd1" -ErrorAction Stop
-  
+
   [void](. $PSScriptRoot\Overrides\Alias.ps1)
   [void](. $PSScriptRoot\Classes\ArgumentCompleter.ps1)
-
 }
 catch {
   throw "Failed to initialize PowerShell profile.`n$($_.Exception.Message)"
