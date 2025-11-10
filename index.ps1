fa8ae00a28aeea9bdba35cab2e242f1a14550a2d
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-10 01:10:31 -0800

    Refactor parameter import syntax in index.ps1

--- index.ps1
+++ index.ps1
@@ -1,3 +1,5 @@
 $Env:PSModulePath += ";$PSScriptRoot\Modules"
-$PSDefaultParameterValues = Import-PowerShellDataFile "$PSScriptRoot\Overrides\Param.psd1" -ErrorAction Stop
+
+$PSDefaultParameterValues = Import-PowerShellDataFile -Path "$PSScriptRoot\Overrides\Param.psd1" -ErrorAction Stop
+
 [void](. $PSScriptRoot\Overrides\Alias.ps1)
