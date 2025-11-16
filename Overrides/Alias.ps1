d1239b816b930ec4b409026334faf9da7a87005c
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-15 22:59:32 -0800

    Refactor ReadOnly and ReadOnlyAll definitions for consistency

--- Overrides/Alias.ps1
+++ Overrides/Alias.ps1
@@ -2,15 +2,9 @@ Set-Alias clear Clear-Line # default: Clear-Host
 Set-Alias rd Remove-Directory # default: Remove-Item
 Set-Alias man Get-HelpOnline # default: Get-Help
 
-$ReadOnly = @{
-  Option = 'ReadOnly'
-}
-$ReadOnlyAll = @{
-  Option = @(
-    'ReadOnly'
-    'AllScope'
-  )
-}
+$Option = , 'Readonly'
+$ReadOnly = @{ Option = $Option }
+$ReadOnlyAll = @{ Option = $Option + 'AllScope' }
 
 New-Alias clip Set-Clipboard @ReadOnly # default: clip.exe
 New-Alias verb Get-VerbPowerShell @ReadOnly # default: implicit Get-Verb
