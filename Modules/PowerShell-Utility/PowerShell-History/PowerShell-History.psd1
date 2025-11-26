7c5ccaa61b82fa7f98a41aad363a70c13fcaef05
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-26 01:11:54 -0800

    Add RequiredModules section to PowerShell-History module manifest and adjust parameter formatting in Edit-History function

--- Modules/PowerShell-Utility/PowerShell-History/PowerShell-History.psd1
+++ Modules/PowerShell-Utility/PowerShell-History/PowerShell-History.psd1
@@ -2,6 +2,9 @@
   RootModule        = 'PowerShell-History.psm1'
   ModuleVersion     = '1.0.0.0'
   GUID              = '6fd4487b-611b-4c0a-b124-f9c975b542d9'
+  RequiredModules   = @(
+    'Windows-Shell'
+  )
   FunctionsToExport = @(
     'Edit-History'
   )
--- Modules/PowerShell-Utility/PowerShell-History/PowerShell-History.psm1
+++ Modules/PowerShell-Utility/PowerShell-History/PowerShell-History.psm1
@@ -1,10 +1,10 @@
 New-Alias oc Edit-History
 function Edit-History {
   $File = @{
-    Path         = Get-PSReadLineOption |
+    Path        = Get-PSReadLineOption |
       Select-Object -ExpandProperty HistorySavePath
-    ProfileName  = 'PowerShell'
-    CreateWindow = $true
+    ProfileName = 'PowerShell'
+    Window      = $true
   }
 
   Edit-ShellItem @File
