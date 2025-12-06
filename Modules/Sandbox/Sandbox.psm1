114b29dd29f56cfed3ba9477982500e0319f46c6
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-06 15:31:17 -0800

    Add Flag parameter to Test-Sandbox function

--- Modules/Sandbox/Sandbox.psm1
+++ Modules/Sandbox/Sandbox.psm1
@@ -2,9 +2,14 @@ New-Alias sand Sandbox\Test-Sandbox
 New-Alias sandbox Sandbox\Test-Sandbox
 function Test-Sandbox {
   param (
-    [string]$Path
+    [string]$Path,
+    [Parameter(
+      ParameterSetName = 'FlagSet'
+    )]
+    [switch]$Flag
   )
 
   "Path: $Path"
   "Args: $args"
+  'ParameterSet: ' + $PSCmdlet.ParameterSetName
 }
