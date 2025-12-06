16ca145b458586c94ee043da675b1326621c5362
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-06 01:09:31 -0800

    Fix variable naming for read-only aliases in script

--- Overrides/Alias.ps1
+++ Overrides/Alias.ps1
@@ -1,20 +1,23 @@
-$ReadOnly = @{
-  Option = 'Readonly'
+$Readonly = @{
+  Option = 'ReadOnly'
 }
-$ReadOnlyAllScope = @{
-  Option = 'Readonly', 'AllScope'
+$ReadonlyConstant = @{
+  Option = 'ReadOnly', 'Constant'
+}
+$ReadonlyAllScope = @{
+  Option = 'ReadOnly', 'AllScope'
 }
 
 # Reassign PS alias
 Set-Alias clear Shell\Clear-Line # was: Clear-Host
 Set-Alias rd Shell\Remove-Directory # was: Remove-Item
 Set-Alias man PSTool\Get-HelpOnline # was: Get-Help
-Set-Alias gp Git\Get-Repository @ReadOnly # was: Get-Member
-Set-Alias gm Git\Write-Repository @ReadOnly # was: Get-Member
+Set-Alias gp Git\Get-Repository @ReadonlyConstant # was: Get-ItemProperty
+Set-Alias gm Git\Write-Repository @ReadonlyConstant # was: Get-Member
 
 # Mask implicit PS alias
-New-Alias verb PSTool\Get-VerbList @ReadOnly # implicit Get-*
+New-Alias verb PSTool\Get-VerbList @Readonly # implicit Get-*
 
 # Mask native PATH executable
-New-Alias clip Set-Clipboard @ReadOnly # clip.exe
-New-Alias run WindowsSystem\Invoke-CommandPrompt @ReadOnlyAllScope # nvm/run.cmd
+New-Alias clip Set-Clipboard @Readonly # clip.exe
+New-Alias run WindowsSystem\Invoke-CommandPrompt @ReadonlyAllScope # nvm/run.cmd
