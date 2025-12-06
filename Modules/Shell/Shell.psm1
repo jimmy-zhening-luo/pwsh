f60c35c47bf983f1d6fe962fd9b218f3206669dd
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-06 00:14:52 -0800

    Remove unused System.IO namespace import in Shell.psm1

--- Modules/Shell/Shell.psm1
+++ Modules/Shell/Shell.psm1
@@ -1,6 +1,5 @@
 using namespace System.Collections
 using namespace System.Collections.Generic
-using namespace System.IO
 using namespace System.Management.Automation
 using namespace System.Management.Automation.Language
 
