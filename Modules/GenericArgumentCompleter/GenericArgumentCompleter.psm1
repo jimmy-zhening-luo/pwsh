3a2d403cbbb6f8656e422acaa72d2aeebed00696
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-05 21:24:20 -0800

    Add case handling to GenericCompletionsAttribute class

--- Modules/GenericArgumentCompleter/GenericArgumentCompleter.psm1
+++ Modules/GenericArgumentCompleter/GenericArgumentCompleter.psm1
@@ -5,31 +5,70 @@ using namespace System.Management.Automation.Language
 
 class GenericCompletionsAttribute : ArgumentCompleterAttribute, IArgumentCompleterFactory {
   [string] $Units
+  [string] $Case
 
   GenericCompletionsAttribute(
     [string] $units
   ) {
     $this.Units = $units
+    $this.Case = 'Lower'
+  }
+  GenericCompletionsAttribute(
+    [string] $units,
+    [string] $case
+  ) {
+    $this.Units = $units
+    $this.Case = $case
   }
 
   [IArgumentCompleter] Create() {
     return [GenericCompleter]::new(
-      $this.Units
+      $this.Units,
+      $this.Case
     )
   }
 }
 
 class GenericCompleter : IArgumentCompleter {
   [string] $Units
+  [string] $Case
 
   GenericCompleter(
-    [string] $units
+    [string] $units,
+    [string] $case
   ) {
+    if (
+      -not $case -or $case -notin @(
+        'Lower'
+        'Upper'
+        'Preserve'
+      )
+    ) {
+      throw [ArgumentException]::new('case')
+    }
+
     if (-not $units) {
       throw [ArgumentException]::new('units')
     }
 
+    $unitKeys = (
+      $units -split ','
+    ).Trim().ToLowerInvariant() -notmatch '^\s*$'
+
+    if (-not $unitKeys) {
+      throw [ArgumentException]::new('units')
+    }
+
+    $unique = @()
+    $unique += $unitKeys |
+      Select-Object -Unique
+
+    if (-not $unique -or $unique.Count -ne $unitKeys.Count) {
+      throw [ArgumentException]::new('units')
+    }
+
     $this.Units = $units
+    $this.Case = $case
   }
 
   [IEnumerable[CompletionResult]] CompleteArgument(
@@ -40,9 +79,20 @@ class GenericCompleter : IArgumentCompleter {
     [IDictionary] $fakeBoundParameters
   ) {
 
-    $Local:units = ($this.Units -split ',').Trim().ToLowerInvariant() |
-      ? { -not [string]::IsNullOrWhiteSpace($_) } |
-      Select-Object -Unique
+    $Local:units = (
+      $this.Units -split ','
+    ).Trim() -notmatch '^\s*$'
+
+    switch ($this.Case) {
+      Lower {
+        $Local:units = $Local:units.ToLowerInvariant()
+        break
+      }
+      Upper {
+        $Local:units = $Local:units.ToUpperInvariant()
+      }
+    }
+
     $unitMatches = @()
 
     if ($Local:units) {
