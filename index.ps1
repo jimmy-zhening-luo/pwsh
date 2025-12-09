74baa3ac319a1627ca38c85fe146838e26a9a2e9
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-08 18:16:48 -0800

    Refactor alias import in index.ps1 file

--- index.ps1
+++ index.ps1
@@ -2,4 +2,4 @@ $Env:PSModulePath += ";$PSScriptRoot\Modules"
 
 $PSDefaultParameterValues = Import-PowerShellDataFile -Path $PSScriptRoot\Overrides\Parameter.psd1
 
-[void](. $PSScriptRoot\Overrides\Alias.ps1)
+. $PSScriptRoot\Overrides\Alias.ps1
