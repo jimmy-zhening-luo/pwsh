d6f1926b911345df3ef564eab4fb91b6f0f0fec1
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-12 20:33:58 -0800

    Add Cmdlet project with Hello world implementation and solution file

---
+++ Cmdlet/Good/Good.cs
@@ -0,0 +1,15 @@
+﻿using System.Management.Automation;
+
+namespace Good;
+
+[Cmdlet(
+  VerbsCommon.Get,
+  "Hello"
+)]
+public class Hello : Cmdlet
+{
+  protected override void ProcessRecord()
+  {
+    WriteObject("Hello, World!");
+  }
+}
