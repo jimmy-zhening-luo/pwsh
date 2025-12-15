38e96731ffd803fad6bedf104ace37b90e066a5d
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-14 19:44:07 -0800

    Refactor namespace and remove unused code in PathCompleter.cs

--- Cmdlet/PathCompleter/PathCompleter.cs
+++ Cmdlet/PathCompleter/PathCompleter.cs
@@ -1,58 +1,12 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Management.Automation;
-using System.Management.Automation.Language;
-using System.Text.RegularExpressions;
 
-namespace Completer
+namespace PathCompleter
 {
   public enum PathItemType
   {
     Any,
     File,
     Directory
-  }
-
-  public abstract class PathCompleter : CompleterBase
-  {
-    public static string EasyDirectorySeparator = "/";
-
-    public static string NormalDirectorySeparator = @"\";
-
-    public static string DuplicateDirectorySeparatorPattern = @"(?<!^)\\+";
-
-    public abstract List<string> FindPathCompletion(
-      string typedPath
-    );
-
-    public override List<string> FulfillCompletion(
-      string parameterName,
-      string wordToComplete,
-      IDictionary fakeBoundParameters
-    )
-    {
-      string normalizedWordToComplete = Unescape(wordToComplete).Replace(
-        EasyDirectorySeparator,
-        NormalDirectorySeparator
-      );
-      string typedPath = Regex.Replace(
-        normalizedWordToComplete,
-        DuplicateDirectorySeparatorPattern,
-        NormalDirectorySeparator
-      );
-
-      List<string> completionPaths = FindPathCompletion(
-        typedPath
-      );
-      List<string> completions = new List<string>();
-
-      foreach (string completion in completionPaths)
-      {
-        completions.Add(completion);
-      }
-
-      return completions;
-    }
   }
 }
