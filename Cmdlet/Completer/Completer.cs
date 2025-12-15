af474943c9295490bd9bfc2b1f3aedde7d5f67d6
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-14 23:49:17 -0800

    Fix syntax error in list initialization

--- Cmdlet/Completer/Completer.cs
+++ Cmdlet/Completer/Completer.cs
@@ -249,7 +249,7 @@ namespace Completer
     public Completer Create() {
       Collection<PSObject> generator = Generator.Invoke();
 
-      List<string> span = new List<string>;
+      List<string> span = new List<string>();
 
       foreach (PSObject wrapper in generator)
       {
