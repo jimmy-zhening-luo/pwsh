ce66fa004f3ed7b98ae2ce619dbc385935d81083
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-14 23:25:32 -0800

    Add using directive for ObservableCollection in Completer.cs

--- Cmdlet/Completer/Completer.cs
+++ Cmdlet/Completer/Completer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Management.Automation;
 using System.Management.Automation.Language;
 
