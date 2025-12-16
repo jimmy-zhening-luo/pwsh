086875486c7b4db4ce2cb3fad657457b1ca50525
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-16 01:00:01 -0800

    Add initial Transform.cs file with namespace structure

---
+++ Class/Transform/Transform.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Management.Automation.Language;
+using System.Text.RegularExpressions;
+
+namespace PathCompleter
+{
+  namespace Commands
+  {
+
+  }
+}
