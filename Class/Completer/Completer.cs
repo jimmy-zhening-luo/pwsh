4310cb35392a8a326c292e6e7b1a9e9d68f68c99
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-16 12:44:19 -0800

    Remove unused namespaces from Completer.cs

--- Class/Completer/Completer.cs
+++ Class/Completer/Completer.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Management.Automation;
 using System.Management.Automation.Language;
 
