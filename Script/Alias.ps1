99bb074b13f11b237de1e0e58ae43ee6a1d8b208
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-16 12:42:56 -0800

    Fix alias references in Alias.ps1 to use PSHelp module

--- Script/Alias.ps1
+++ Script/Alias.ps1
@@ -13,14 +13,14 @@
 #region Reassign
 Set-Alias clear Shell\Clear-Line # was: Microsoft.PowerShell.Core\Clear-Host
 Set-Alias rd Shell\Remove-Directory # was: Remove-Item
-Set-Alias man PSTool\Get-HelpOnline # was: Microsoft.PowerShell.Core\Get-Help
+Set-Alias man PSHelp\Get-HelpOnline # was: Microsoft.PowerShell.Core\Get-Help
 Set-Alias gp Shell\Get-GitRepository @Readonly -Force # was: Get-ItemProperty
 Set-Alias gm Shell\Write-GitRepository @Readonly -Force # was: Get-Member
 #endregion
 
 #region Mask
 # Implicit PowerShell alias
-New-Alias verb PSTool\Get-VerbList @Readonly # was: implicit Get-Verb
+New-Alias verb PSHelp\Get-VerbList @Readonly # was: implicit Get-Verb
 
 # PATH executable
 New-Alias clip Set-Clipboard @Readonly # was: clip.exe
