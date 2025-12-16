4c1177e5e211dbb46d31f257f9889a1acc9d677a
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-16 12:44:55 -0800

    Remove unused namespaces from PathCompleter.cs

--- Class/PathCompleter/PathCompleter.cs
+++ Class/PathCompleter/PathCompleter.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Management.Automation;
-using System.Management.Automation.Language;
 using System.Text.RegularExpressions;
 
 namespace PathCompleter
