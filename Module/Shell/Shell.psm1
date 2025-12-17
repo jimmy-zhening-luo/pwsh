ba6342e56f064eac15919a28eed8d3f06e74bd63
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-16 22:42:40 -0800

    Remove unused namespace from Shell.psm1

--- Module/Shell/Shell.psm1
+++ Module/Shell/Shell.psm1
@@ -1,6 +1,5 @@
 using namespace System.Collections.Generic
 using namespace System.Management.Automation
-using namespace System.Management.Automation.Language
 using namespace Completer.PathCompleter
 
 class PathCompletionsAttribute : ArgumentCompleterAttribute, IArgumentCompleterFactory {
