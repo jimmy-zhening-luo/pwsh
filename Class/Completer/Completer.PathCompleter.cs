049189579c97097d53fa5640ada8530f177b0630
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-16 18:57:10 -0800

    Refactor TestPathCompleter class to enhance path completion logic and improve code clarity

--- Class/Completer/Completer.PathCompleter.cs
+++ Class/Completer/Completer.PathCompleter.cs
@@ -1,5 +1,12 @@
 using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Management.Automation;
+using System.Management.Automation.Language;
+using System.Text.RegularExpressions;
+using Typed;
 
 namespace Completer
 {
@@ -28,5 +35,197 @@ namespace Completer
       Function
     }
 
+    public abstract class TestPathCompleter : CompleterBase
+    {
+      private readonly string Root;
+      private readonly PathItemType Type;
+      private readonly bool Flat;
+      private readonly bool UseNativePathSeparator;
+
+      public TestPathCompleter(
+        string root,
+        PathItemType type,
+        bool flat,
+        bool useNativePathSeparator
+      )
+      {
+        Root = root;
+        Type = type;
+        Flat = flat;
+        UseNativePathSeparator = useNativePathSeparator;
+      }
+
+      public override List<string> FulfillCompletion(
+        string parameterName,
+        string wordToComplete,
+        IDictionary fakeBoundParameters
+      )
+      {
+        List<string> completions = [];
+
+        string fullRoot = Path.GetFullPath(Root);
+
+        bool constrainToDirectories = (
+          Type == PathItemType.Directory
+        );
+        bool constrainToFiles = (
+          Type == PathItemType.File
+        );
+
+        string currentPathValue = Regex.Replace(
+          Typed.Typed
+            .Unescape(
+              wordToComplete
+            )
+            .Replace(
+              TypedPath.FriendlyPathSeparatorChar,
+              TypedPath.PathSeparatorChar
+            ),
+          TypedPath.DuplicatePathSeparatorPattern,
+          TypedPath.PathSeparator
+        );
+
+        List<string> currentDirectoryValue = [];
+        List<string> fragment = [];
+        List<string> searchLocation = [];
+        List<string> searchFilter = [];
+
+        if (!string.IsNullOrWhiteSpace(currentPathValue))
+        {
+          int lastSeparatorIndex = currentPathValue.LastIndexOf(
+            TypedPath.PathSeparatorChar
+          );
+
+          if (lastSeparatorIndex >= 0)
+          {
+            string beforeSeparator = currentPathValue[..lastSeparatorIndex];
+            string afterSeparator = lastSeparatorIndex + 1 < currentPathValue.Length
+              ? currentPathValue[(lastSeparatorIndex + 1)..]
+              : string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(beforeSeparator))
+            {
+              currentDirectoryValue.Add(beforeSeparator);
+            }
+
+            if (!string.IsNullOrWhiteSpace(afterSeparator))
+            {
+              fragment.Add(afterSeparator);
+            }
+          }
+          else
+          {
+            fragment.Add(currentPathValue);
+          }
+
+          if (currentDirectoryValue.Count != 0)
+          {
+            string testPath = Path.GetFullPath(currentDirectoryValue[0], fullRoot);
+
+            if (Directory.Exists(testPath))
+            {
+              searchLocation.Add(testPath);
+
+            }
+          }
+
+          if (fragment.Count != 0)
+          {
+            searchFilter.Add(
+              fragment[0] + "*"
+            );
+          }
+        }
+
+        if (searchLocation.Count == 0)
+        {
+          searchLocation.Add(fullRoot);
+        }
+
+        if (searchFilter.Count == 0)
+        {
+          searchFilter.Add("*");
+        }
+
+        List<string> matchedDirectoryNames = [];
+        List<string> matchedDirectoryFiles = [];
+
+        if (constrainToDirectories || !constrainToFiles)
+        {
+          string[] directories = Directory.GetDirectories(
+            searchLocation[0],
+            searchFilter[0],
+            SearchOption.TopDirectoryOnly
+          );
+
+          if (directories.Length != 0)
+          {
+            matchedDirectoryNames.AddRange(directories);
+          }
+        }
+
+        if (constrainToFiles || !constrainToDirectories)
+        {
+          string[] files = Directory.GetFiles(
+            searchLocation[0],
+            searchFilter[0],
+            SearchOption.TopDirectoryOnly
+          );
+
+          if (files.Length != 0)
+          {
+            matchedDirectoryFiles.AddRange(files);
+          }
+        }
+
+        string prepender = currentDirectoryValue.Count == 0
+          ? string.Empty
+          : currentDirectoryValue[0] + TypedPath.PathSeparator;
+
+        List<string> prependedDirectories = prepender == string.Empty
+          ? matchedDirectoryNames
+          : [
+            .. matchedDirectoryNames.Select(
+              directory => prepender + directory
+            )
+          ];
+        List<string> prependedFiles = prepender == string.Empty
+          ? matchedDirectoryFiles
+          : [
+            .. matchedDirectoryFiles.Select(
+              file => prepender + file
+            )
+          ];
+
+        List<string> appendedDirectories = Flat
+          ? prependedDirectories
+          : [
+            .. prependedDirectories.Select(
+              directory => directory.EndsWith(
+                TypedPath.PathSeparator
+              )
+                ? directory
+                : directory + TypedPath.PathSeparator
+            )
+          ];
+
+        List<string> unnormalizedCompletions = [
+          .. appendedDirectories,
+          .. prependedFiles
+        ];
+        completions.AddRange(
+          UseNativePathSeparator
+            ? unnormalizedCompletions
+            : unnormalizedCompletions.Select(
+                item => item.Replace(
+                  TypedPath.PathSeparatorChar,
+                  TypedPath.FriendlyPathSeparatorChar
+                )
+              )
+        );
+
+        return completions;
+      }
+    }
   }
 }
