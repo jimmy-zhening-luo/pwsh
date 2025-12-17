bce6f8e612e27e68086b8cf06d0d27349b18206a
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-17 02:22:26 -0800

    Refactor parameter validation and types in Install.ps1

--- Script/Install.ps1
+++ Script/Install.ps1
@@ -7,127 +7,104 @@ param (
     Mandatory,
     Position = 0
   )]
-  [ValidateNotNullOrEmpty()]
+  [ValidateNotNullOrWhiteSpace()]
   [string]$ClassRoot,
 
   [Parameter(
     Mandatory,
     Position = 1
   )]
-  [ValidateNotNullOrEmpty()]
+  [ValidateNotNullOrWhiteSpace()]
   [string]$ModuleRoot,
 
-  [Parameter(
-    Mandatory
-  )]
+  [Parameter(Mandatory)]
   [AllowEmptyCollection()]
   [ValidateNotNull()]
-  [hashtable[]]$Types,
+  [string[]]$Types,
 
-  [Parameter(
-    Mandatory
-  )]
+  [Parameter(Mandatory)]
   [AllowEmptyCollection()]
   [ValidateNotNull()]
-  [hashtable[]]$Modules
+  [string[]]$Modules
 
 )
 
-#region Modules
+[string]$Private:DOTNET_RUNTIME = 'net9.0'
+
+
+#region Installer
+function Install-PSProject {
+  [CmdletBinding()]
+  [OutputType([string])]
+  param(
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrWhiteSpace()]
+    [string]$Project,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrWhiteSpace()]
+    [string]$InstallPath
+  )
+
+  [string]$Private:BuildOutput = "$ClassRoot\$Project\bin\Release\$DOTNET_RUNTIME\$Project.dll"
+
+  if (Test-Path -Path $BuildOutput) {
+    [string]$Private:InstalledAssembly = "$InstallPath\$Project.dll"
+
+    if (
+      -not (
+        Test-Path -Path $InstalledAssembly -PathType Leaf
+      ) -or (
+        Get-FileHash -Path $InstalledAssembly
+      ).Hash -ne (
+        Get-FileHash -Path $BuildOutput
+      ).Hash
+    ) {
+      [hashtable]$Private:Install = @{
+        Path        = $BuildOutput
+        Destination = $InstallPath
+        Force       = $True
+        ErrorAction = 'Continue'
+      }
+      Copy-Item @Install
+    }
+  }
+  else {
+    Write-Warning -Message "Project '$Project' is not built, skipping."
+  }
+}
+#endregion
+
+
+#region Install/Module
 foreach ($Private:Module in $Modules) {
-  [string]$Private:Name = $Module.Name
-  [string]$Private:Runtime = $Module.Runtime
-
   [hashtable]$Private:ModuleDistro = @{
-    Destination = "$ModuleRoot\$Name"
-    Force       = $True
+    Project     = $Module
+    InstallPath = "$ModuleRoot\$Module"
   }
-  [hashtable]$Private:ModuleAssembly = @{
-    Path = "$($ModuleDistro.Destination)\$Name.dll"
-  }
-
-  [hashtable]$Private:Built = @{
-    Path = "$ClassRoot\$Name\bin\Release\$Runtime\$Name.dll"
-  }
-  if (Test-Path @Built) {
-    if (
-      -not (
-        Test-Path @ModuleAssembly -PathType Leaf
-      ) -or (
-        Get-FileHash @ModuleAssembly
-      ).Hash -ne (
-        Get-FileHash @Built
-      ).Hash
-    ) {
-      try {
-        Copy-Item @Built @ModuleDistro
-      }
-      catch {
-        [hashtable]$Private:Exception = @{
-          Message   = "Failed to install $Name to $($ModuleAssembly.Path)."
-          Exception = $PSItem.Exception
-        }
-        Write-Error @Exception
-      }
-    }
-  }
-  else {
-    Write-Warning -Message "$Name has not been built, skipping."
-  }
+  Install-PSProject @ModuleDistro
 }
 #endregion
 
-#region Types
-foreach ($Private:Type in $Types) {
-  [string]$Private:Name = $Type.Name
-  [string]$Private:Runtime = $Type.Runtime
 
+#region Install/Type
+foreach ($Private:Type in $Types) {
   [hashtable]$Private:TypeDistro = @{
-    Destination = $ClassRoot
-    Force       = $True
+    Project     = $Type
+    InstallPath = $ClassRoot
   }
-  [hashtable]$Private:TypeAssembly = @{
-    Path = "$($TypeDistro.Destination)\$Name.dll"
-  }
-
-  [hashtable]$Private:Built = @{
-    Path = "$ClassRoot\$Name\bin\Release\$Runtime\$Name.dll"
-  }
-  if (Test-Path @Built) {
-    if (
-      -not (
-        Test-Path @TypeAssembly -PathType Leaf
-      ) -or (
-        Get-FileHash @TypeAssembly
-      ).Hash -ne (
-        Get-FileHash @Built
-      ).Hash
-    ) {
-      try {
-        Copy-Item @Built @TypeDistro
-      }
-      catch {
-        [hashtable]$Private:Exception = @{
-          Message   = "Failed to install $Name to $($TypeAssembly.Path)."
-          Exception = $PSItem.Exception
-        }
-        Write-Error @Exception
-      }
-    }
-  }
-  else {
-    Write-Warning -Message "$Name has not been built, skipping."
-  }
+  Install-PSProject @TypeDistro
 }
 #endregion
 
-#region Accelerate
+
+#region Load Type
 foreach ($Private:Type in $Types) {
-  [hashtable]$Private:Assembly = @{
-    Path = "$ClassRoot\$($Type.Name).dll"
+  [string]$Private:TypeAssembly = @{
+    Path = "$ClassRoot\$Type.dll"
   }
-  if (Test-Path @Assembly -PathType Leaf) {
-    Add-Type @Assembly
+  if (Test-Path @TypeAssembly -PathType Leaf) {
+    Add-Type @TypeAssembly
   }
 }
 #endregion
