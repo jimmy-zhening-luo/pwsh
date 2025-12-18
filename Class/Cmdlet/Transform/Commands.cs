a2c028ad2419683f539a33d3297df874347205c6
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-17 23:50:36 -0800

    Add ConvertToHex and TestCmdlet classes; remove Transform.cs; slightly update hex enumeration efficiency and input validation.

---
+++ Class/Cmdlet/Transform/Commands.cs
@@ -0,0 +1,52 @@
+using System.Linq;
+using System.Management.Automation;
+
+namespace Transform
+{
+  namespace Commands
+  {
+    [Cmdlet(VerbsData.ConvertTo, "Hex")]
+    [OutputType(typeof(string[]))]
+    public class ConvertToHex : Cmdlet
+    {
+      [Parameter(
+        Mandatory = true,
+        Position = 0,
+        ValueFromPipeline = true,
+        ValueFromPipelineByPropertyName = true,
+        ValueFromRemainingArguments = true
+      )]
+      [AllowEmptyCollection]
+      [Alias("Integer")]
+      [ValidateRange(0, long.MaxValue)]
+      public long[] Number
+      {
+        get => numbers;
+        set => numbers = value;
+      }
+      private long[] numbers;
+
+      [Parameter]
+      [Alias("Case")]
+      public SwitchParameter Lowercase
+      {
+        get => lowercase;
+        set => lowercase = value;
+      }
+      private bool lowercase;
+
+      protected override void ProcessRecord()
+      {
+        foreach (long number in numbers)
+        {
+          string hex = number.ToString("X");
+          string print = lowercase
+            ? hex.ToLower()
+            : hex;
+
+          WriteObject(print, true);
+        }
+      }
+    }
+  }
+}
---
+++ Class/Cmdlet/Transform/Test.cs
@@ -0,0 +1,31 @@
+using System.Management.Automation;
+
+namespace Transform
+{
+  namespace Commands
+  {
+    namespace Test
+    {
+      [Cmdlet(VerbsDiagnostic.Test, "Cmdlet")]
+      [OutputType(typeof(string))]
+      public class TestCmdlet : Cmdlet
+      {
+        [Parameter(
+          Position = 0
+        )]
+        [AllowEmptyString]
+        public string Name
+        {
+          get => name;
+          set => name = value;
+        }
+        private string name = "";
+
+        protected override void EndProcessing()
+        {
+          WriteObject(name);
+        }
+      }
+    }
+  }
+}
