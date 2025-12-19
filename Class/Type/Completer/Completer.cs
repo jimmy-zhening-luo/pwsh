ff55d69a44759ae114ce67977bf6b177278e152a
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-18 17:17:51 -0800

    Add Completer class with domain and surrounding properties

--- Class/Type/Completer/Completer.cs
+++ Class/Type/Completer/Completer.cs
@@ -7,6 +7,82 @@ using System.Management.Automation.Language;
 
 namespace Completer
 {
+  internal class Completer : CompleterBase
+  {
+    public readonly IEnumerable<string> Domain;
+    public readonly bool Surrounding;
+    public Completer(
+      IEnumerable<string> domain,
+      CompletionCase casing,
+      bool surrounding
+    ): base(casing)
+    {
+      Domain = domain;
+      Surrounding = surrounding;
+    }
+    public IEnumerable<string> FindCompletion(
+      string wordToComplete
+    )
+    {
+      string unescapedWordToComplete = Typed.Typed.Unescape(wordToComplete);
+      if (
+        string.IsNullOrWhiteSpace(
+          unescapedWordToComplete
+        )
+      )
+      {
+        foreach (string member in Domain)
+        {
+          yield return member;
+        }
+        yield break;
+      }
+      else
+      {
+        int matched = 0;
+        foreach (string member in Domain)
+        {
+          if (
+            member.StartsWith(
+              unescapedWordToComplete,
+              StringComparison.OrdinalIgnoreCase
+            )
+          )
+          {
+            ++matched;
+            yield return member;
+          }
+        }
+        if (Surrounding && matched <= 1)
+        {
+          foreach (string member in Domain)
+          {
+            if (
+              member.Length > unescapedWordToComplete.Length
+              && member.IndexOf(
+                unescapedWordToComplete,
+                1,
+                StringComparison.OrdinalIgnoreCase
+              ) >= 1
+            )
+            {
+              yield return member;
+            }
+          }
+        }
+        yield break;
+      }
+    }
+    protected override IEnumerable<string> FulfillArgumentCompletion(
+      string parameterName,
+      string wordToComplete,
+      IDictionary fakeBoundParameters
+    )
+    {
+      return FindCompletion(wordToComplete);
+    }
+  }
+
   [AttributeUsage(AttributeTargets.Parameter)]
   public class StaticCompletionsAttribute(
     string StringifiedDomain,
@@ -48,82 +124,6 @@ namespace Completer
         Casing ?? CompletionCase.Preserve,
         Surrounding ?? true
       );
-    }
-
-    internal class Completer : CompleterBase
-    {
-      public readonly IEnumerable<string> Domain;
-      public readonly bool Surrounding;
-      public Completer(
-        IEnumerable<string> domain,
-        CompletionCase casing,
-        bool surrounding
-      ): base(casing)
-      {
-        Domain = domain;
-        Surrounding = surrounding;
-      }
-      public IEnumerable<string> FindCompletion(
-        string wordToComplete
-      )
-      {
-        string unescapedWordToComplete = Typed.Typed.Unescape(wordToComplete);
-        if (
-          string.IsNullOrWhiteSpace(
-            unescapedWordToComplete
-          )
-        )
-        {
-          foreach (string member in Domain)
-          {
-            yield return member;
-          }
-          yield break;
-        }
-        else
-        {
-          int matched = 0;
-          foreach (string member in Domain)
-          {
-            if (
-              member.StartsWith(
-                unescapedWordToComplete,
-                StringComparison.OrdinalIgnoreCase
-              )
-            )
-            {
-              ++matched;
-              yield return member;
-            }
-          }
-          if (Surrounding && matched <= 1)
-          {
-            foreach (string member in Domain)
-            {
-              if (
-                member.Length > unescapedWordToComplete.Length
-                && member.IndexOf(
-                  unescapedWordToComplete,
-                  1,
-                  StringComparison.OrdinalIgnoreCase
-                ) >= 1
-              )
-              {
-                yield return member;
-              }
-            }
-          }
-          yield break;
-        }
-      }
-      protected override IEnumerable<string> FulfillArgumentCompletion(
-        string parameterName,
-        string wordToComplete,
-        IDictionary fakeBoundParameters
-      )
-      {
-        return FindCompletion(wordToComplete);
-      }
     }
   }
 }
