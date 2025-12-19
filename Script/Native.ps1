ca5ef0b7deed3ed99fe3e1244840bee24b3089d4
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-18 23:16:13 -0800

    flatten native completer loading

--- Script/Native.ps1
+++ Script/Native.ps1
@@ -1,1 +1,8 @@
-. $PSScriptRoot\NativeCompletion\Dotnet.ps1
+#region Dotnet
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+  dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+  }
+}
+#endregion
--- Script/NativeCompletion/Dotnet.ps1
+++
@@ -1,6 +0,0 @@
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-  param($wordToComplete, $commandAst, $cursorPosition)
-  dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
-    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-  }
-}
