d660fabf7c6b083a7ee52f8c10b82faa1d4d0f89
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-19 19:46:24 -0800

    Add checks before modifying environment variables

--- index.ps1
+++ index.ps1
@@ -1,11 +1,13 @@
-$Env:PSModulePath += ";$PSScriptRoot\Module"
+if (-not $Env:PSModulePath.EndsWith(";$PSScriptRoot\Module")) {
+  $Env:PSModulePath += ";$PSScriptRoot\Module"
+}
 
-$Global:PSDefaultParameterValues = Import-PowerShellDataFile -Path $PSScriptRoot\Data\Parameter.psd1
+if (-not $Global:PSDefaultParameterValue) {
+  $Global:PSDefaultParameterValues = Import-PowerShellDataFile -Path $PSScriptRoot\Data\Parameter.psd1
+}
 
 $Script:REPO_ROOT = Split-Path $PSScriptRoot
 
 . $PSScriptRoot\Script\Install.ps1
 . $PSScriptRoot\Script\Alias.ps1
 . $PSScriptRoot\Script\Native.ps1
-
-Write-Output $env:SSH_CLIENT
