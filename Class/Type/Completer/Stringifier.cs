15f24bab6736a5a0a2d34655c91f9f8680a22b16
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-21 14:51:37 -0800

    Refactor Stringifier methods for concise syntax

--- Class/Type/Completer/Stringifier.cs
+++ Class/Type/Completer/Stringifier.cs
@@ -4,28 +4,22 @@ namespace Completer
 {
   public static class Stringifier
   {
-    public static string Unescape(string escapedText)
-    {
-      return (
-        escapedText.Length > 1
-        && escapedText.StartsWith('\'')
-        && escapedText.EndsWith('\'')
-      )
-        ? escapedText[1..^1]
-          .Replace(
-            "''",
-            "'"
-          )
-        : escapedText;
-    }
+    public static string Unescape(string escapedText) => (
+      escapedText.Length > 1
+      && escapedText.StartsWith('\'')
+      && escapedText.EndsWith('\'')
+    )
+      ? escapedText[1..^1]
+        .Replace(
+          "''",
+          "'"
+        )
+      : escapedText;
 
-    public static string Escape(string text)
-    {
-      return text.Contains(' ')
-        ? "'"
-          + CodeGeneration.EscapeSingleQuotedStringContent(text)
-          + "'"
-        : text;
-    }
+    public static string Escape(string text) => text.Contains(' ')
+      ? "'"
+        + CodeGeneration.EscapeSingleQuotedStringContent(text)
+        + "'"
+      : text;
   } // class Stringifier
 } // namespace Completer
