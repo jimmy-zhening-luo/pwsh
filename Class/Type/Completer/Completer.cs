8998cd62d03729ab4d3f47c056f55495675bb098
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-20 22:33:02 -0800

    Remove unnecessary using directive for System.Management.Automation.Language in Completer.cs

--- Class/Type/Completer/Completer.cs
+++ Class/Type/Completer/Completer.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
-using System.Management.Automation.Language;
 
 namespace Completer
 {
