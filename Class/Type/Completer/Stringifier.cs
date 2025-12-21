e51f7528125e475e558eb7118e712daec0254fbf
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-20 23:13:29 -0800

    Refactor Stringifier to simplify character handling

--- Class/Type/Completer/Stringifier.cs
+++ Class/Type/Completer/Stringifier.cs
@@ -4,33 +4,27 @@ namespace Completer
 {
   public static class Stringifier
   {
-    public readonly static char SpaceChar = ' ';
-    public readonly static char QuoteChar = '\'';
-    public readonly static string Space = " ";
-    public readonly static string Quote = "'";
-    public readonly static string EscapedQuote = "''";
-
     public static string Unescape(string escapedText)
     {
       return (
         escapedText.Length > 1
-        && escapedText.StartsWith(QuoteChar)
-        && escapedText.EndsWith(QuoteChar)
+        && escapedText.StartsWith('\'')
+        && escapedText.EndsWith('\'')
       )
         ? escapedText[1..^1]
           .Replace(
-            EscapedQuote,
-            Quote
+            "''",
+            "'"
           )
         : escapedText;
     }
 
     public static string Escape(string text)
     {
-      return text.Contains(SpaceChar)
-        ? Quote
+      return text.Contains(' ')
+        ? "'"
           + CodeGeneration.EscapeSingleQuotedStringContent(text)
-          + Quote
+          + "'"
         : text;
     }
   } // class Stringifier
