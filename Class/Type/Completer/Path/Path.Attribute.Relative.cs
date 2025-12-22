162050347ec3646122353d6d0aabca812858b24a
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-21 16:26:52 -0800

    nah, doesn't work

--- Class/Type/Completer/Path/Path.Attribute.Relative.cs
+++ Class/Type/Completer/Path/Path.Attribute.Relative.cs
@@ -6,38 +6,49 @@ namespace Completer
   namespace PathCompleter
   {
     [AttributeUsage(AttributeTargets.Parameter)]
-    public class RelativePathCompletionsAttribute : PathCompletionsAttribute
+    public class RelativePathCompletionsAttribute : BaseCompletionsAttribute
     {
-      public RelativePathCompletionsAttribute(ScriptBlock currentDirectory) : this(
-        currentDirectory,
-        PathItemType.Any,
-        false
-      )
-      { }
+      public readonly ScriptBlock CurrentDirectory;
+      public readonly PathItemType ItemType;
+      public readonly bool Flat;
+
+      private RelativePathCompletionsAttribute() : base() { }
+
+      public RelativePathCompletionsAttribute(
+        ScriptBlock currentDirectory
+      ) : this()
+      {
+        CurrentDirectory = currentDirectory;
+      }
 
       public RelativePathCompletionsAttribute(
         ScriptBlock currentDirectory,
         PathItemType itemType
-      ) : this(
-        currentDirectory,
-        itemType,
-        false
-      )
-      { }
+      ) : this(currentDirectory)
+      {
+        ItemType = itemType;
+      }
 
       public RelativePathCompletionsAttribute(
         ScriptBlock currentDirectory,
         PathItemType itemType,
         bool flat
-      ) : base(
-        currentDirectory
-          .Invoke()[0]
-          .BaseObject
-          .ToString(),
-        itemType,
-        flat
-      )
-      { }
+      ) : this(currentDirectory, itemType)
+      {
+        Flat = flat;
+      }
+
+      public override PathCompleter Create()
+      {
+        return new PathCompleter(
+          CurrentDirectory
+            .Invoke()[0]
+            .BaseObject
+            .ToString(),
+          ItemType,
+          Flat
+        );
+      }
     }
   } // namespace PathCompleter
 } // namespace Completer
