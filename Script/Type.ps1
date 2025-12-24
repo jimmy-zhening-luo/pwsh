8060142e159a249612082d0a81cf8a22f59619e7
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-24 00:30:56 -0800

    Remove List type accelerator from Type.ps1

--- Script/Type.ps1
+++ Script/Type.ps1
@@ -2,5 +2,4 @@
   $TypeAcceleratorsClass = [psobject].Assembly.GetType('System.Management.Automation.TypeAccelerators')
 
   $TypeAcceleratorsClass::Add('path', [System.IO.Path])
-  $TypeAcceleratorsClass::Add('List', [System.Collections.Generic.List])
 }
