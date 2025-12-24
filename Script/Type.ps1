3e77d80d76f0f12f175367e920e6783bcdb195a8
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-23 23:36:57 -0800

    Add List type accelerator to Type.ps1 script

--- Script/Type.ps1
+++ Script/Type.ps1
@@ -1,5 +1,6 @@
 & {
   $TypeAcceleratorsClass = [psobject].Assembly.GetType('System.Management.Automation.TypeAccelerators')
-  
+
   $TypeAcceleratorsClass::Add('path', [System.IO.Path])
+  $TypeAcceleratorsClass::Add('List', [System.Collections.Generic.List])
 }
