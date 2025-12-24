bc3c8bd8ce511d0b50f9e574d0b7afbbf0164d4c
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-24 02:37:33 -0800

    ```

--- Module/Shell/Shell.Remove/Shell.Remove.psm1
+++ Module/Shell/Shell.Remove/Shell.Remove.psm1
@@ -5,15 +5,75 @@ using namespace Completer.PathCompleter
 .FORWARDHELPCATEGORY Cmdlet
 #>
 function Remove-Directory {
-
+  [CmdletBinding(
+    DefaultParameterSetName = 'Path',
+    SupportsShouldProcess,
+    SupportsTransactions,
+    ConfirmImpact = 'Medium'
+  )]
   [OutputType([void])]
   param(
 
+    [Parameter(
+      ParameterSetName = 'Path',
+      Mandatory,
+      Position = 0,
+      ValueFromPipeline,
+      ValueFromPipelineByPropertyName
+    )]
+    [SupportsWildcards()]
     [RelativePathCompletions(
       { return $PWD.Path }
     )]
-    [string]$Path
+    [string[]]$Path,
+
+    [Parameter(
+      ParameterSetName = 'LiteralPath',
+      Mandatory,
+      ValueFromPipelineByPropertyName
+    )]
+    [Alias('PSPath', 'LP')]
+    [string[]]$LiteralPath,
+
+    [SupportsWildcards()]
+    [string]$Filter,
+
+    [SupportsWildcards()]
+    [string[]]$Include,
+
+    [SupportsWildcards()]
+    [string[]]$Exclude
   )
 
-  Remove-Item -Path $Path -Recurse -Force
+  begin {
+    $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('Remove-Item', [System.Management.Automation.CommandTypes]::Cmdlet)
+    $scriptCmd = { & $wrappedCmd -Recurse -Force @PSBoundParameters }
+    $steppablePipeline = $scriptCmd.GetSteppablePipeline()
+
+    if (
+      $PSCmdlet.ShouldProcess(
+        $Value,
+        "Open Transaction: rm -rf $Path, $LiteralPath"
+      )
+    ) {
+      $steppablePipeline.Begin($PSCmdlet)
+    }
+  }
+
+  process {
+    if (
+      $PSCmdlet.ShouldProcess(
+        $Value,
+        "> Step: Remove-Item -Recurse -Force -Path [[$Path]] -LiteralPath [$LiteralPath] -- " + (ConvertTo-Json $PSBoundParameters -EnumsAsStrings -Depth 6)
+      )
+    ) {
+      $steppablePipeline.Process($PSItem)
+    }
+  }
+
+  end {
+    if ($PSCmdlet.ShouldProcess('Transaction', 'Close')) {
+      $steppablePipeline.End()
+    }
+  }
 }
--- Module/Shell/Shell.Set/Shell.Set.psm1
+++ Module/Shell/Shell.Set/Shell.Set.psm1
@@ -1,116 +1,222 @@
 using namespace Completer.PathCompleter
 
 function Set-Directory {
-
-  [OutputType([void])]
+  [CmdletBinding(
+    DefaultParameterSetName = 'Path'
+  )]
+  [OutputType(
+    [System.Management.Automation.PathInfo],
+    [System.Management.Automation.PathInfoStack]
+  )]
   param(
-
+    [Parameter(
+      ParameterSetName = 'Path',
+      Position = 0,
+      ValueFromPipeline,
+      ValueFromPipelineByPropertyName
+    )]
+    [SupportsWildcards()]
     [RelativePathCompletions(
       { return $PWD.Path },
       [PathItemType]::Directory
     )]
-    [string]$Path
+    [AllowEmptyString()]
+    [AllowNull()]
+    [string]$Path,
+
+    [Parameter(
+      ParameterSetName = 'LiteralPath',
+      Mandatory,
+      ValueFromPipelineByPropertyName
+    )]
+    [Alias('PSPath', 'LP')]
+    [string]$LiteralPath,
+
+    [Parameter(
+      ParameterSetName = 'Stack',
+      ValueFromPipelineByPropertyName
+    )]
+    [AllowEmptyString()]
+    [AllowNull()]
+    [string]$Stack,
+
+    [switch]$PassThru
   )
 
-  if ($Path) {
-    Set-Location -Path $Path @args
+  begin {
+    $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('Set-Location', [System.Management.Automation.CommandTypes]::Cmdlet)
+
+    if (-not $Path -and -not $LiteralPath) {
+      $PSBoundParameters.Path = Split-Path $PWD.Path
+    }
+    $scriptCmd = { & $wrappedCmd @PSBoundParameters }
+    $steppablePipeline = $scriptCmd.GetSteppablePipeline()
+    $steppablePipeline.Begin($PSCmdlet)
   }
-  elseif ($args) {
-    Set-Location @args
+
+  process {
+    $steppablePipeline.Process($PSItem)
   }
-  else {
-    Set-Location -Path (Split-Path $PWD.Path)
+
+  end {
+    $steppablePipeline.End()
   }
 }
 
 function Set-DirectorySibling {
-
-  [OutputType([void])]
+  [OutputType(
+    [System.Management.Automation.PathInfo]
+  )]
   param(
-
+    [Parameter(
+      Position = 0,
+      ValueFromPipeline,
+      ValueFromPipelineByPropertyName
+    )]
+    [SupportsWildcards()]
     [RelativePathCompletions(
       { return Split-Path $PWD.Path },
       [PathItemType]::Directory
     )]
-    [string]$Path
+    [AllowEmptyString()]
+    [AllowNull()]
+    [string]$Path,
+
+    [switch]$PassThru
   )
 
-  Set-Location -Path (Join-Path (Split-Path $PWD.Path) $Path) @args
+  $PSBoundParameters.Path = Join-Path (Split-Path $PWD.Path) $Path
+  Set-Directory @PSBoundParameters
 }
 
 function Set-DirectoryRelative {
-
-  [OutputType([void])]
+  [OutputType(
+    [System.Management.Automation.PathInfo]
+  )]
   param(
-
+    [Parameter(
+      Position = 0,
+      ValueFromPipeline,
+      ValueFromPipelineByPropertyName
+    )]
+    [SupportsWildcards()]
     [RelativePathCompletions(
       { return $PWD.Path | Split-Path | Split-Path },
       [PathItemType]::Directory
     )]
-    [string]$Path
+    [AllowEmptyString()]
+    [AllowNull()]
+    [string]$Path,
+
+    [switch]$PassThru
   )
 
-  Set-Location -Path (Join-Path ($PWD.Path | Split-Path | Split-Path) $Path) @args
+  $PSBoundParameters.Path = Join-Path ($PWD.Path | Split-Path | Split-Path) $Path
+  Set-Directory @PSBoundParameters
 }
 
 function Set-DirectoryHome {
-
-  [OutputType([void])]
+  [OutputType(
+    [System.Management.Automation.PathInfo]
+  )]
   param(
-
+    [Parameter(
+      Position = 0,
+      ValueFromPipeline,
+      ValueFromPipelineByPropertyName
+    )]
+    [SupportsWildcards()]
     [PathCompletions(
       '~',
       [PathItemType]::Directory
     )]
-    [string]$Path
+    [AllowEmptyString()]
+    [AllowNull()]
+    [string]$Path,
+
+    [switch]$PassThru
   )
 
-  Set-Location -Path (Join-Path $HOME $Path) @args
+  $PSBoundParameters.Path = Join-Path $HOME $Path
+  Set-Directory @PSBoundParameters
 }
 
 function Set-DirectoryCode {
-
-  [OutputType([void])]
+  [OutputType(
+    [System.Management.Automation.PathInfo]
+  )]
   param(
-
+    [Parameter(
+      Position = 0,
+      ValueFromPipeline,
+      ValueFromPipelineByPropertyName
+    )]
+    [SupportsWildcards()]
     [PathCompletions(
       '~\code',
       [PathItemType]::Directory
     )]
-    [string]$Path
+    [AllowEmptyString()]
+    [AllowNull()]
+    [string]$Path,
+
+    [switch]$PassThru
   )
 
-  Set-Location -Path (Join-Path $REPO_ROOT $Path) @args
+  $PSBoundParameters.Path = Join-Path $REPO_ROOT $Path
+  Set-Directory @PSBoundParameters
 }
 
 function Set-Drive {
-
-  [OutputType([void])]
+  [OutputType(
+    [System.Management.Automation.PathInfo]
+  )]
   param(
-
+    [Parameter(
+      Position = 0,
+      ValueFromPipeline,
+      ValueFromPipelineByPropertyName
+    )]
+    [SupportsWildcards()]
     [RelativePathCompletions(
       { return $PWD.Drive.Root },
       [PathItemType]::Directory
     )]
-    [string]$Path
+    [AllowEmptyString()]
+    [AllowNull()]
+    [string]$Path,
+
+    [switch]$PassThru
   )
 
-  Set-Location -Path (Join-Path $PWD.Drive.Root $Path) @args
+  $PSBoundParameters.Path = Join-Path $PWD.Drive.Root $Path
+  Set-Directory @PSBoundParameters
 }
 
 function Set-DriveD {
-
-  [OutputType([void])]
+  [OutputType(
+    [System.Management.Automation.PathInfo]
+  )]
   param(
-
+    [Parameter(
+      Position = 0,
+      ValueFromPipeline,
+      ValueFromPipelineByPropertyName
+    )]
+    [SupportsWildcards()]
     [PathCompletions(
       'D:',
       [PathItemType]::Directory
     )]
-    [string]$Path
+    [AllowEmptyString()]
+    [AllowNull()]
+    [string]$Path,
+
+    [switch]$PassThru
   )
 
-  Set-Location -Path (Join-Path D: $Path) @args
+  $PSBoundParameters.Path = Join-Path D: $Path
+  Set-Directory @PSBoundParameters
 }
 
 New-Alias c Set-Directory
