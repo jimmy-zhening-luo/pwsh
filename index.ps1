676f620fe30fc75d0f02f8a6abc7eb5f3cb9fdb4
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-27 00:05:51 -0800

    Refactor SSH_CLIENT check in index.ps1

--- index.ps1
+++ index.ps1
@@ -2,15 +2,14 @@ if (-not $Env:PSModulePath.EndsWith(";$PSScriptRoot\Module;")) {
   $Env:PSModulePath += ";$PSScriptRoot\Module;"
 }
 
-if (-not -not $Env:SSH_CLIENT) {
-  $PSSessionOption.NoCompression = $True
-}
-
 $Global:PSDefaultParameterValues = Import-PowerShellDataFile -Path $PSScriptRoot\Data\Parameter.psd1
 
 $Script:REPO_ROOT = Split-Path $PSScriptRoot
 
 . $PSScriptRoot\Script\Install.ps1
 . $PSScriptRoot\Script\Alias.ps1
-# . $PSScriptRoot\Script\Key.ps1
 . $PSScriptRoot\Script\Native.ps1
+
+if (-not -not $Env:SSH_CLIENT) {
+  . $PSScriptRoot\Script\Key.ps1
+}
