bddda58f48d3ad578c2998d4db0a7242ab0aa620
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-29 14:04:11 -0800

    Reorder script loading and environment variable checks in index.ps1 for improved clarity

--- index.ps1
+++ index.ps1
@@ -1,15 +1,15 @@
-if (-not $Env:PSModulePath.EndsWith(";$PSScriptRoot\Module;")) {
-  $Env:PSModulePath += ";$PSScriptRoot\Module;"
+. $PSScriptRoot\Script\Alias.ps1
+. $PSScriptRoot\Script\Native.ps1
+. $PSScriptRoot\Script\Install.ps1
+
+if (-not -not $Env:SSH_CLIENT) {
+  . $PSScriptRoot\Script\Key.ps1
 }
 
 $Global:PSDefaultParameterValues = Import-PowerShellDataFile -Path $PSScriptRoot\Data\Parameter.psd1
 
 $Script:REPO_ROOT = Split-Path $PSScriptRoot
 
-. $PSScriptRoot\Script\Install.ps1
-. $PSScriptRoot\Script\Alias.ps1
-. $PSScriptRoot\Script\Native.ps1
-
-if (-not -not $Env:SSH_CLIENT) {
-  . $PSScriptRoot\Script\Key.ps1
+if (-not $Env:PSModulePath.EndsWith(";$PSScriptRoot\Module;")) {
+  $Env:PSModulePath += ";$PSScriptRoot\Module;"
 }
