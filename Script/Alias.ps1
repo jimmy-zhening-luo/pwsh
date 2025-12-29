c17ebd7cc15ba2151e57b595be35106c3e5b61bd
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-29 13:58:59 -0800

    remove alias gu

--- Script/Alias.ps1
+++ Script/Alias.ps1
@@ -8,11 +8,14 @@ Remove-Alias rd
 # was: Get-Help
 Remove-Alias man
 
+# was: Get-Member
+Remove-Alias gm -Force
+
 # was: Get-ItemProperty
 Remove-Alias gp -Force
 
-# was: Get-Member
-Remove-Alias gm -Force
+# was: Get-Unique
+Remove-Alias gu -Force
 
 #endregion
 
