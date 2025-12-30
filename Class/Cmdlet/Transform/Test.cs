19637a077c494ff7b7010cf9f4dc312b9a51a2f5
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-29 23:15:03 -0800

    Add AllowNull attribute to Path field in Test.cs

--- Class/Cmdlet/Transform/Test.cs
+++ Class/Cmdlet/Transform/Test.cs
@@ -19,6 +19,7 @@ namespace Transform
           Position = 0
         )]
         [AllowEmptyString]
+        [AllowNull]
         public string Path;
 
         protected override void EndProcessing()
