d84a204a08260f5b7170d48fc3cdad89a37abee4
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2026-01-01 23:24:47 -0800

    Add Core command classes and interface implementation

---
+++ Class/Cmdlet/Core/Core.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Management.Automation;
+
+namespace Core
+{
+  public abstract class Command : Cmdlet, ICommand
+  { }
+
+  public abstract class PSCommand : PSCmdlet, ICommand
+  { }
+
+  public interface ICommand
+  {
+    protected static bool Ssh() => Environment.GetEnvironmentVariable(
+      "SSH_CLIENT"
+    ) != null;
+  }
+} // namespace Core
