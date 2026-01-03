7476de3a2e2df38726f74f8e011e13a9e5d1a944
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2026-01-02 18:19:46 -0800

    restructure aux cmdlets

--- Class/Cmdlet/Auxiliary/Test.cs
+++ Class/Cmdlet/Auxiliary/Test.cs
@@ -2,55 +2,49 @@ using System.Management.Automation;
 
 namespace Auxiliary
 {
-  namespace Test
+  [Cmdlet(
+    VerbsDiagnostic.Test,
+    "Cmdlet",
+    HelpUri = "https://learn.microsoft.com/dotnet/api/system.management.automation.cmdlet"
+  )]
+  [Alias("test")]
+  [OutputType(typeof(string))]
+  public class TestCmdlet : PSCmdlet
   {
-    namespace Commands
+    [Parameter(
+      Position = 0
+    )]
+    [AllowEmptyString]
+    public string Path
     {
-      [Cmdlet(
-        VerbsDiagnostic.Test,
-        "Cmdlet",
-        HelpUri = "https://learn.microsoft.com/dotnet/api/system.management.automation.cmdlet"
-      )]
-      [Alias("test")]
-      [OutputType(typeof(string))]
-      public class TestCmdlet : PSCmdlet
+      get => path;
+      set => path = value;
+    }
+    private string path = string.Empty;
+
+    [Parameter(DontShow = true)]
+    public SwitchParameter z;
+
+    protected override void EndProcessing()
+    {
+      string pwd = SessionState.Path.CurrentLocation.Path;
+
+      WriteObject(
+        pwd,
+        true
+      );
+
+      string rel = Path == string.Empty
+        ? pwd
+        : System.IO.Path.GetRelativePath(pwd, Path);
+
+      if (System.IO.Path.Exists(rel))
       {
-        [Parameter(
-          Position = 0
-        )]
-        [AllowEmptyString]
-        public string Path
-        {
-          get => path;
-          set => path = value;
-        }
-        private string path = string.Empty;
-
-        [Parameter(DontShow = true)]
-        public SwitchParameter z;
-
-        protected override void EndProcessing()
-        {
-          string pwd = SessionState.Path.CurrentLocation.Path;
-
-          WriteObject(
-            pwd,
-            true
-          );
-
-          string rel = Path == string.Empty
-            ? pwd
-            : System.IO.Path.GetRelativePath(pwd, Path);
-
-          if (System.IO.Path.Exists(rel))
-          {
-            WriteObject(
-              System.IO.Path.GetFullPath(Path, pwd),
-              true
-            );
-          }
-        }
+        WriteObject(
+          System.IO.Path.GetFullPath(Path, pwd),
+          true
+        );
       }
     }
-  } // namespace Test
-} // namespace Auxiliary
+  }
+}
