89c9dabda37689fd19cb9a2609a22a6579bb9a85
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-07 17:23:29 -0800

    Refactor PSReadLine key handler paths in Key.ps1

--- Script/Key.ps1
+++ Script/Key.ps1
@@ -1,2 +1,2 @@
-Set-PSReadLineKeyHandler -Chord Shift+DownArrow -Function NextHistory
-Set-PSReadLineKeyHandler -Chord Shift+UpArrow -Function PreviousHistory
+PSReadLine\Set-PSReadLineKeyHandler -Chord Shift+DownArrow -Function NextHistory
+PSReadLine\Set-PSReadLineKeyHandler -Chord Shift+UpArrow -Function PreviousHistory
