c812d394951938a46b42096789e5ae807ea7f1fc
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-10 14:44:05 -0800

    Add abstract class for WinGet application command

--- Class/Module/Windows/App/WinGet.cs
+++ Class/Module/Windows/App/WinGet.cs
@@ -3,4 +3,8 @@ namespace Module.Windows.App.Commands
   using System.Diagnostics;
   using System.Management.Automation;
 
+  public abstract class WinGetAppCommand : PSCoreCommand
+  {
+    public static string WinGet() => Context.LocalAppData() + @"\Microsoft\WindowsApps\winget.exe";
+  }
 }
