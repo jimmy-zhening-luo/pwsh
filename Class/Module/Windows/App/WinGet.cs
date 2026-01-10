d16ba33869b25d626511327deb6c56716af90221
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-10 15:16:55 -0800

    +check stat

--- Class/Module/Windows/App/WinGet.cs
+++ Class/Module/Windows/App/WinGet.cs
@@ -1,10 +1,34 @@
 namespace Module.Windows.App.Commands
 {
+  using System;
   using System.Diagnostics;
   using System.Management.Automation;
 
   public abstract class WinGetAppCommand : PSCoreCommand
   {
     public static string WinGet() => Context.LocalAppData() + @"\Microsoft\WindowsApps\winget.exe";
+
+    public void CheckStatus()
+    {
+      var status = Var("LASTEXITCODE");
+
+      if (
+        status != null
+        && (
+          status.GetType() == typeof(string)
+          && status != string.Empty
+          && status != "0"
+          && status != "1"
+          || status.GetType() == typeof(int)
+          && status != 0
+          && status != 1
+        )
+      )
+      {
+        throw new InvalidOperationException(
+          "winget.exe error, execution stopped with exit code: " + (string)status
+        );
+      }
+    }
   }
 }
