feb9a4048b8839d74778160a371ea09bf3736ee5
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-10 19:37:09 -0800

    Add StartHistory Cmdlet for history management

---
+++ Class/Module/Profile/Profile.cs
@@ -0,0 +1,17 @@
+namespace Module.Profile.Commands
+{
+  [Cmdlet(
+    VerbsLifecycle.Start,
+    "History"
+  )]
+  [Alias("oc")]
+  [OutputType(typeof(void))]
+  public class StartHistory : Cmdlet
+  {
+    protected override void EndProcessing() => Context.CreateProcess(
+      Context.LocalAppData() + @"\Programs\Microsoft VS Code\bin\code.cmd",
+      Context.AppData() + @"\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt --profile=Setting --new-window",
+      true
+    );
+  }
+}
