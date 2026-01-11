ca26a4907f2857e2764bb12804e82bb68eb60e21
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-10 19:53:06 -0800

    Refactor command line arguments for clarity

--- Class/Module/Profile/History.cs
+++ Class/Module/Profile/History.cs
@@ -11,8 +11,13 @@ namespace Module.Profile.Commands
   public class StartHistory : Cmdlet
   {
     protected override void EndProcessing() => Context.CreateProcess(
-      Context.LocalAppData() + @"\Programs\Microsoft VS Code\bin\code.cmd",
-      Context.AppData() + @"\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt --profile=Setting --new-window",
+      Context.LocalAppData(
+        @"\Programs\Microsoft VS Code\bin\code.cmd"
+      ),
+      Context.AppData(
+        @"\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
+      )
+        + " --profile=Setting --new-window",
       true
     );
   }
