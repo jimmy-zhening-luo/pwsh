251d812854a42f136452ae431fb12381f564e3fc
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-11 20:03:45 -0800

    Remove SetDirectoryHome class and related attributes

--- Class/Module/Shell/Set/Directory.Location.Derived.cs
+++ Class/Module/Shell/Set/Directory.Location.Derived.cs
@@ -96,36 +96,6 @@ namespace Module.Shell.Set.Commands
 
   [Cmdlet(
     VerbsCommon.Set,
-    "DirectoryHome",
-    SupportsTransactions = true,
-    HelpUri = "https://go.microsoft.com/fwlink/?LinkID=2097049"
-  )]
-  [Alias("ch")]
-  [OutputType(
-    typeof(System.Management.Automation.PathInfo)
-  )]
-  public class SetDirectoryHome : SetDirectoryLocation
-  {
-    [Parameter(
-      Position = 0,
-      ValueFromPipeline = true,
-      ValueFromPipelineByPropertyName = true
-    )]
-    [AllowEmptyString]
-    [SupportsWildcards]
-    [PathCompletions(
-      "~",
-      PathItemType.Directory
-    )]
-    public new string Path;
-
-    protected override string location() => "";
-
-    protected override string root() => Context.Home();
-  }
-
-  [Cmdlet(
-    VerbsCommon.Set,
     "DirectoryCode",
     SupportsTransactions = true,
     HelpUri = "https://go.microsoft.com/fwlink/?LinkID=2097049"
