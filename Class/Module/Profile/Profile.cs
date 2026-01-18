c60be7ffc04f69d9bd05192f864988b6c453d121
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-18 15:28:57 -0800

    Refactor EndProcessing method to include null check for REPO_ROOT variable

--- Class/Module/Profile/Profile.cs
+++ Class/Module/Profile/Profile.cs
@@ -12,17 +12,31 @@ namespace Module.Profile
     [OutputType(typeof(void))]
     public class StartProfile : CoreCommand
     {
-      protected override void EndProcessing() => Context.CreateProcess(
-        Context.LocalAppData(
-          @"Programs\Microsoft VS Code\bin\code.cmd"
-        ),
-        System.IO.Path.GetFullPath(
-          "pwsh",
-          ((PSObject)Var("REPO_ROOT")).BaseObject.ToString()
-        )
-          + " --profile=Default",
-        true
-      );
+      protected override void EndProcessing()
+      {
+        var repoRoot = ((PSObject)Var("REPO_ROOT")).BaseObject.ToString();
+
+        if (repoRoot == null)
+        {
+          Throw(
+            "REPO_ROOT environment variable is not set.",
+            "PSProfileGlobalVariableNotSet",
+            ErrorCategory.ResourceUnavailable
+          );
+        }
+
+        Context.CreateProcess(
+          Context.LocalAppData(
+            @"Programs\Microsoft VS Code\bin\code.cmd"
+          ),
+          System.IO.Path.GetFullPath(
+            "pwsh",
+            repoRoot
+          )
+            + " --profile=Default",
+          true
+        );
+      }
     }
   }
 }
