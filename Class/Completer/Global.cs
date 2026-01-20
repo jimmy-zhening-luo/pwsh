60832ac85b21ebf63f9b48b0d97a809a56656ddf
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-19 23:00:13 -0800

    Add IDictionary alias for easier reference in Global.cs

--- Class/Completer/Global.cs
+++ Class/Completer/Global.cs
@@ -3,6 +3,7 @@ global using static System.Management.Automation.RunspaceMode;
 global using static Completer.Escaper;
 
 global using AttributeUsage = System.AttributeUsageAttribute;
+global using IDictionary = System.Collections.IDictionary;
 global using IStringEnumerable = System.Collections.Generic.IEnumerable<string>;
 global using ICompletionEnumerable = System.Collections.Generic.IEnumerable<System.Management.Automation.CompletionResult>;
 global using CommandAst = System.Management.Automation.Language.CommandAst;
