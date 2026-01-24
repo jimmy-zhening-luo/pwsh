27f66a271f517726aa0c1ea18a9351fa51fbc19e
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-24 11:34:11 -0800

    Add WrappedCommandShouldProcess class implementation

---
+++ Class/Command/WrappedCommandShouldProcess.cs
@@ -0,0 +1,14 @@
+namespace Module.Command;
+
+public abstract class WrappedCommandShouldProcess(
+  string WrappedCommandName
+) : WrappedCommand(WrappedCommandName)
+{
+  [Parameter]
+  [Alias("wi")]
+  public SwitchParameter? WhatIf;
+
+  [Parameter]
+  [Alias("cf")]
+  public SwitchParameter? Confirm;
+}
