5288a1f1c9fc4c93c64cdf93e1f30e76a16a6a75
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-25 17:52:57 -0800

    Add StartWorkspace command classes with parameters

---
+++ Class/Command/Shell/Start/Workspace/Commands/StartWorkspace.cs
@@ -0,0 +1,137 @@
+namespace Module.Command.Shell.Start.Workspace.Commands;
+
+[Cmdlet(
+  VerbsLifecycle.Start,
+  "Workspace",
+  DefaultParameterSetName = "Path",
+  SupportsTransactions = true,
+  HelpUri = "https://code.visualstudio.com/docs/configure/command-line#_core-cli-options"
+)]
+[Alias("i")]
+[OutputType(typeof(void))]
+public sealed class StartWorkspace : VirtualStartWorkspace
+{
+  [Parameter(
+    Position = 0
+  )]
+  [AllowEmptyString]
+  [PathCompletions]
+  public new string? Path;
+}
+
+[Cmdlet(
+  VerbsLifecycle.Start,
+  "WorkspaceSibling",
+  DefaultParameterSetName = "Path",
+  SupportsTransactions = true,
+  HelpUri = "https://code.visualstudio.com/docs/configure/command-line#_core-cli-options"
+)]
+[Alias("i.", "ix")]
+[OutputType(typeof(void))]
+public sealed class StartWorkspaceSibling : VirtualStartWorkspace
+{
+  [Parameter(
+    Position = 0
+  )]
+  [AllowEmptyString]
+  [PathCompletions(
+    ".."
+  )]
+  public new string? Path;
+
+  private protected sealed override string LocationSubpath => "..";
+}
+
+[Cmdlet(
+  VerbsLifecycle.Start,
+  "WorkspaceRelative",
+  DefaultParameterSetName = "Path",
+  SupportsTransactions = true,
+  HelpUri = "https://code.visualstudio.com/docs/configure/command-line#_core-cli-options"
+)]
+[Alias("i..", "ixx")]
+[OutputType(typeof(void))]
+public sealed class StartWorkspaceRelative : VirtualStartWorkspace
+{
+  [Parameter(
+    Position = 0
+  )]
+  [AllowEmptyString]
+  [PathCompletions(
+    @"..\.."
+  )]
+  public new string? Path;
+
+  private protected sealed override string LocationSubpath => @"..\..";
+}
+
+[Cmdlet(
+  VerbsLifecycle.Start,
+  "WorkspaceHome",
+  DefaultParameterSetName = "Path",
+  SupportsTransactions = true,
+  HelpUri = "https://code.visualstudio.com/docs/configure/command-line#_core-cli-options"
+)]
+[Alias("ih")]
+[OutputType(typeof(void))]
+public sealed class StartWorkspaceHome : VirtualStartWorkspace
+{
+  [Parameter(
+    Position = 0
+  )]
+  [AllowEmptyString]
+  [PathCompletions(
+    "~"
+  )]
+  public new string? Path;
+
+  private protected sealed override string Location => Home();
+}
+
+[Cmdlet(
+  VerbsLifecycle.Start,
+  "WorkspaceCode",
+  DefaultParameterSetName = "Path",
+  SupportsTransactions = true,
+  HelpUri = "https://code.visualstudio.com/docs/configure/command-line#_core-cli-options"
+)]
+[Alias("ic")]
+[OutputType(typeof(void))]
+public sealed class StartWorkspaceCode : VirtualStartWorkspace
+{
+  [Parameter(
+    Position = 0
+  )]
+  [AllowEmptyString]
+  [PathCompletions(
+    @"~\code"
+  )]
+  public new string? Path;
+
+  private protected sealed override string Location => Home();
+
+  private protected sealed override string LocationSubpath => "code";
+}
+
+[Cmdlet(
+  VerbsLifecycle.Start,
+  "WorkspaceDrive",
+  DefaultParameterSetName = "Path",
+  SupportsTransactions = true,
+  HelpUri = "https://code.visualstudio.com/docs/configure/command-line#_core-cli-options"
+)]
+[Alias("i/")]
+[OutputType(typeof(void))]
+public sealed class StartWorkspaceDrive : VirtualStartWorkspace
+{
+  [Parameter(
+    Position = 0
+  )]
+  [AllowEmptyString]
+  [PathCompletions(
+    @"\"
+  )]
+  public new string? Path;
+
+  private protected sealed override string Location => Drive();
+}
---
+++ Class/Command/Shell/Start/Workspace/VirtualStartWorkspace.cs
@@ -0,0 +1,133 @@
+namespace Module.Command.Shell.Start.Workspace;
+
+public abstract class VirtualStartWorkspace : CoreCommand
+{
+  [Parameter(
+    Position = 0
+  )]
+  [AllowEmptyString]
+  public string? Path;
+
+  [Parameter(
+    Position = 1
+  )]
+  [AllowEmptyString]
+  public string Name
+  {
+    get => profileName;
+    set => profileName = value;
+  }
+  private string profileName = "";
+
+  [Parameter(
+    Position = 2,
+    ValueFromRemainingArguments = true,
+    DontShow = true
+  )]
+  [AllowEmptyCollection]
+  public string[] Argument
+  {
+    get => arguments;
+    set => arguments = value;
+  }
+  private string[] arguments = [];
+
+  [Parameter]
+  public SwitchParameter Window
+  {
+    get => window;
+    set => window = value;
+  }
+  private bool window;
+
+  [Parameter]
+  public SwitchParameter ReuseWindow
+  {
+    get => reuseWindow;
+    set => reuseWindow = value;
+  }
+  private bool reuseWindow;
+
+  private protected sealed override bool NoSsh => true;
+
+  private protected sealed override void TransformParameters()
+  {
+    if (string.IsNullOrEmpty(Path))
+    {
+      Path = Here
+        ? Pwd()
+        : Reanchor();
+    }
+    else if (!Here)
+    {
+      Path = Reanchor(Path);
+    }
+  }
+
+  private protected sealed override void AfterEndProcessing()
+  {
+    var argumentList = new List<string>()
+    {
+      .. arguments
+    };
+
+    argumentList.Insert(
+      0,
+      Path!
+    );
+
+    if (!string.IsNullOrEmpty(profileName))
+    {
+      if (
+        profileName.StartsWith("--")
+      )
+      {
+        argumentList.Add(
+          profileName
+        );
+      }
+      else
+      {
+        if (profileName == "se")
+        {
+          argumentList.Add(
+            "--profile=Setting"
+          );
+        }
+        else
+        {
+          WriteWarning(
+            "Profiles not supported except for se == Setting lmaoo."
+          );
+        }
+      }
+    }
+
+    if (window)
+    {
+      argumentList.Add(
+        "--new-window"
+      );
+    }
+    else if (reuseWindow)
+    {
+      argumentList.Add(
+        "--reuse-window"
+      );
+    }
+
+    CreateProcess(
+      Path.GetFullPath(
+        @"Programs\Microsoft VS Code\bin\code.cmd",
+        Env(
+          "LOCALAPPDATA"
+        )
+      ),
+      string.Join(
+        ' ',
+        argumentList
+      ),
+      true
+    );
+  }
+}
