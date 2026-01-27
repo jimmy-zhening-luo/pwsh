4508aff830f0acc99409a70c7f297d416d64882d
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-26 20:05:23 -0800

    refactor: add global using directive for System.Management.Automation.Runspaces

--- Class/Using/Using.cs
+++ Class/Using/Using.cs
@@ -1,3 +1,4 @@
 global using System.Collections.Generic;
 global using System.Collections.ObjectModel;
 global using System.Management.Automation;
+global using System.Management.Automation.Runspaces;
