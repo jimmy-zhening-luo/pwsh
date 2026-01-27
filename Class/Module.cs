eb6d485a3dacd86c769c9509c1082dd12626c346
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-26 18:45:34 -0800

    Add back module.cs for native namespace flattening

---
+++ Class/Module.cs
@@ -0,0 +1,7 @@
+global using static Module.Context;
+global using static Module.Input.Escaper;
+global using static Module.Input.PathResolution.Canonicalizer;
+
+global using Module.Input.PathResolution;
+global using Module.Completer;
+global using Module.Completer.PathCompleter;
--- Class/Using/Static.cs
+++ Class/Using/Static.cs
@@ -1,4 +1,1 @@
 global using static System.IO.Path;
-global using static Module.Context;
-global using static Module.Input.Escaper;
-global using static Module.Input.PathResolution.Canonicalizer;
