f4e13b3c92f632c68cff4568b568b6dd7c675892
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-28 16:56:27 -0800

    Remove abstract Path property from VirtualStartWorkspace

--- Class/Command/Shell/Start/Workspace/VirtualStartWorkspace.cs
+++ Class/Command/Shell/Start/Workspace/VirtualStartWorkspace.cs
@@ -2,7 +2,6 @@ namespace Module.Command.Shell.Start.Workspace;
 
 public abstract class VirtualStartWorkspace : CoreCommand
 {
-  public abstract string Path;
   private protected string path = "";
 
   [Parameter(
