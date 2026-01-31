5c7283b9c369db9b49d3e1b1617f6d873be8d563
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-31 14:08:16 -0800

    Remove VSCode property implementation from Application.cs

--- Class/Application.cs
+++ Class/Application.cs
@@ -2,11 +2,8 @@ namespace Module;
 
 internal static class Application
 {
-  internal static string VSCode
-  {
-    get => vscode ??= LocalAppData(
-      @"Programs\Microsoft VS Code\bin\code.cmd"
-    );
-  }
+  internal static string VSCode => vscode ??= LocalAppData(
+    @"Programs\Microsoft VS Code\bin\code.cmd"
+  );
   private static string? vscode;
 }
