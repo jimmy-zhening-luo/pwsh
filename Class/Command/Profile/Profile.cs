c0086427019162c58dc480274633c85bb6766000
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-02 20:20:24 -0800

    Refactor AfterEndProcessing method

--- Class/Command/Profile/Profile.cs
+++ Class/Command/Profile/Profile.cs
@@ -12,32 +12,12 @@ public sealed class StartProfile : CoreCommand
 
   private protected sealed override void AfterEndProcessing()
   {
-    var repoRootVariable = Var(
-      "REPO_ROOT"
+    Invocation.CreateProcess(
+      Application.VSCode,
+      Code(
+        "pwsh"
+      ),
+      true
     );
-
-    if (
-      repoRootVariable is PSObject repoRootObject
-      && repoRootObject.BaseObject is string repoRoot
-    )
-    {
-      Invocation.CreateProcess(
-        Application.VSCode,
-        GetFullPath(
-          "pwsh",
-          repoRoot
-        ),
-        true
-      );
-    }
-    else
-    {
-      Throw(
-        "Expected global Powershell variable '$REPO_ROOT' was not found",
-        "PSProfileGlobalVariableNotSet",
-        ErrorCategory.ObjectNotFound,
-        repoRootVariable
-      );
-    }
   }
 }
