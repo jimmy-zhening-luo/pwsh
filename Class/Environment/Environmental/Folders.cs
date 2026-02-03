1b39439ae3e57f69f6f2774d0dd09bce12db2ced
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-02 18:39:17 -0800

    Update ProgramFiles reference in Folders.cs

--- Class/Environment/Environmental/Folders.cs
+++ Class/Environment/Environmental/Folders.cs
@@ -14,7 +14,7 @@ internal static partial class Environmental
   internal static string ProgramFiles(
     string subpath = ""
   ) => EnvPath(
-    global::ProgramFiles,
+    using::ProgramFiles,
     subpath
   );
 
