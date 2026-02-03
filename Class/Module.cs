c4c502166ec93e84b497614d730c2182ce065040
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-02 18:46:19 -0800

    Remove unused global using directives in Module.cs

--- Class/Module.cs
+++ Class/Module.cs
@@ -1,8 +1,4 @@
-global using static Module.Environment.Environmental.Environmental;
-global using static Module.Input.Escaper;
-global using static Module.Input.Path.Normalizer;
 
-global using Module.Environment;
 global using Module.Input.Path;
 global using Module.Input.Completer;
 global using Module.Input.Completer.PathCompleter;
