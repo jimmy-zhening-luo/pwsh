ccdc09280a1097aa3275102b79fc52136af1307f
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-08 00:46:22 -0800

    ok

---
+++ Class/Command/Help/GetHelp.cs
@@ -0,0 +1,39 @@
+namespace Module.Command.Help;
+
+[Cmdlet(
+  VerbsCommon.Get,
+  "HelpOnline",
+  HelpUri = "https://go.microsoft.com/fwlink/?LinkID=2096483"
+)]
+[Alias("man", "m")]
+[OutputType(typeof(object))]
+public sealed class GetHelpOnline : CoreCommand
+{
+  [Parameter(
+    Position = 0,
+    ValueFromRemainingArguments = true
+  )]
+  [Alias("Command")]
+  [SupportsWildcards]
+  [Completions("*")]
+  public string[] Name
+  {
+    get => names;
+    set => names = value;
+  }
+  private string[] names = [];
+
+  [Parameter]
+  public string[] Parameter
+  {
+    get => parameters;
+    set => parameters = value;
+  }
+  private string[] parameters = [];
+
+  private protected sealed override bool ValidateParameters() => names.Length == 0
+    || names.Length == 1
+    && string.IsNullOrEmpty(
+      names[0]
+    );
+}
