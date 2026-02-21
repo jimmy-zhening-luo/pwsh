a9f1bdde58191d9c05fd3d21cd6fc6af1332e1aa
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-21 12:37:29 -0800

    Remove corrupted string data from Application.cs

--- Class/Client/Environment/Known/Application.cs
+++ Class/Client/Environment/Known/Application.cs
@@ -2,13 +2,11 @@ namespace Module.Client.Environment.Known;
 
 internal static class Application
 {
-  internal static string VSCode => vscode ??= Folder.LocalAppData(
+  internal static string VSCode => Folder.LocalAppData(
     @"Programs\Microsoft VS Code\bin\code.cmd"
   );
-  private static string? vscode;
 
-  internal static string Chrome => chrome ??= Folder.ProgramFiles(
+  internal static string Chrome => Folder.ProgramFiles(
     @"Google\Chrome\Application\chrome.exe"
   );
-  private static string? chrome;
 }
