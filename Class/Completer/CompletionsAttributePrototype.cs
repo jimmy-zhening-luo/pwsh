e71d18efb890e1682b5553d0fe87f6220bb2fad1
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-21 15:22:23 -0800

    Add CompletionsAttributePrototype class implementation

---
+++ Class/Completer/CompletionsAttributePrototype.cs
@@ -0,0 +1,41 @@
+namespace Module.Completer;
+
+public abstract class CompletionsAttributePrototype<TDomain> : BaseCompletionsAttribute<Completer>
+{
+  private readonly T Domain;
+
+  private readonly bool Strict;
+
+  public CompletionsAttributePrototype(
+    TDomain domain
+  ) : base() => Domain = domain;
+
+  public CompletionsAttributePrototype(
+    TDomain domain,
+    bool strict
+  ) : this(
+    domain
+  ) => Strict = strict;
+
+  public CompletionsAttributePrototype(
+    TDomain domain,
+    bool strict,
+    CompletionCase casing
+  ) : base(
+    casing
+  ) => (
+    Domain,
+    Strict
+  ) = (
+    domain,
+    strict
+  );
+
+  private protected abstract IEnumerable<string> ResolveDomain();
+
+  public sealed override Completer Create() => new(
+    ResolveDomain(),
+    Strict,
+    Casing
+  );
+}
