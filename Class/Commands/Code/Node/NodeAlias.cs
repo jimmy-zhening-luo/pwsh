e5e0ba9039ea316870dc520963755be62fd51ac9
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-20 18:16:40 -0800

    Add NodeAlias class with command aliases mapping

---
+++ Class/Commands/Code/Node/NodeAlias.cs
@@ -0,0 +1,36 @@
+namespace Module.Commands.Code.Node;
+
+public static partial class NodeVerb
+{
+  public static Dictionary<string, string> Aliases = new () {
+      "issues" = "bugs",
+      "c" = "config",
+      "ddp" = "dedupe",
+      "home" = "docs",
+      "why" = "explain",
+      "create" = "init",
+      "add" = "install",
+      "i" = "install",
+      "in" = "install",
+      "ln" = "link",
+      "cit" = "install-ci-test",
+      "it" = "install-test",
+      "list" = "ls",
+      "author" = "owner",
+      "rb" = "rebuild",
+      "find" = "search",
+      "s" = "search",
+      "se" = "search",
+      "t" = "test",
+      "unlink" = "uninstall",
+      "remove" = "uninstall",
+      "rm" = "uninstall",
+      "r" = "uninstall",
+      "un" = "uninstall",
+      "up" = "update",
+      "upgrade" = "update",
+      "info" = "view",
+      "show" = "view",
+      "v" = "view"
+  };
+}
