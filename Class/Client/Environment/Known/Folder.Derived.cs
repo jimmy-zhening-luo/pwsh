c36bc9650e3237785bd95241550d09f2f52a4db2
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-21 12:56:42 -0800

    Add SystemDrive method to Folder class

--- Class/Client/Environment/Known/Folder.Derived.cs
+++ Class/Client/Environment/Known/Folder.Derived.cs
@@ -2,6 +2,17 @@ namespace Module.Client.Environment.Known;
 
 internal static partial class Folder
 {
+  internal static string SystemDrive(
+    string subpath = ""
+  ) => System.IO.Path.GetFullPath(
+    FileSystem.PathString.Normalize(
+      subpath
+    ),
+    Windows(
+      ".."
+    )
+  );
+
   internal static string Code(
     string subpath = ""
   ) => System.IO.Path.GetFullPath(
