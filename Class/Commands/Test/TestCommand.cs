3b6c3c1df720f1940d6fa35e42f8db18d9f53ad1
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-21 21:01:12 -0800

    Refactor TestCommand class and update parameters

--- Class/Commands/Test/TestCommand.cs
+++ Class/Commands/Test/TestCommand.cs
@@ -2,63 +2,26 @@ namespace Module.Commands.Test;
 
 [Cmdlet(
   VerbsDiagnostic.Test,
-  "Guid"
+  "Command"
 )]
 [Alias("tc")]
-[OutputType(typeof(string))]
-public sealed class CopyGuid : CoreCommand
+[OutputType(typeof(object))]
+public sealed class TestCommand : CoreCommand
 {
   [Parameter(
-    HelpMessage = "Uppercase GUID"
+    Position = 0
   )]
-  [Alias("Case")]
-  public SwitchParameter Uppercase
+  public string Name
   {
-    get => uppercase;
-    set => uppercase = value;
+    get => name;
+    set => name = value;
   }
-  private bool uppercase;
-
-  [Parameter(
-    HelpMessage = "Only copy GUID to clipboard, omit console output"
-  )]
-  public SwitchParameter Silent
-  {
-    get => silent;
-    set => silent = value;
-  }
-  private bool silent;
+  private string name = "Hello, World";
 
   private protected sealed override void AfterEndProcessing()
   {
-    string guid = System.Guid
-      .NewGuid()
-      .ToString(
-        "D"
-      );
-
-    if (uppercase)
-    {
-      guid = guid.ToUpper();
-    }
-
-    if (!silent)
-    {
-      WriteObject(
-        guid
-      );
-    }
-
-    AddCommand(
-      "Set-Clipboard"
-    )
-      .AddParameter(
-        "Value",
-        new string[]
-        {
-          guid
-        }
-      )
-      .Invoke();
+    WriteObject(
+      name
+    );
   }
 }
