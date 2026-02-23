2c71596b9d5d4114ecc9da4d8344cd2f5e6192c9
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-23 02:15:07 -0800

    Add GitArgument class to Git commands module

---
+++ Class/Commands/Code/Git/GitArgument.cs
@@ -0,0 +1,10 @@
+namespace Module.Commands.Code.Git;
+
+public static partial class GitArgument
+{
+  // @"^(?>(?=.*[*=])(?>.+)|-(?>\w|(?>-\w[-\w]*\w)))$"
+  [System.Text.RegularExpressions.GeneratedRegex(
+    @"\\\\"
+  )]
+  public static partial System.Text.RegularExpressions.Regex GitArgumentRegex();
+}
