7f95be96cefaeac7237dc5fcc22e1f1335fda6a7
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-23 16:21:55 -0800

    Add NodePackageVersionCompletionsAttribute class

---
+++ Class/Commands/Code/Node/NodePackageVersionCompletionsAttribute.cs
@@ -0,0 +1,8 @@
+namespace Module.Commands.Code.Node;
+
+public sealed class NodePackageVersionCompletionsAttribute() : EnumCompletionsAttribute(
+  typeof(NodePackageVersion),
+  false,
+  CompletionCase.Preserve
+)
+{ }
