bd050870b46056ee4e1c736f70a63234766310a8
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-24 03:09:39 -0800

    Remove redundant Name property from TestCommand class

--- Class/Commands/Test/TestCommand.cs
+++ Class/Commands/Test/TestCommand.cs
@@ -11,12 +11,7 @@ public sealed class TestCommand : CoreCommand
   [Parameter(
     Position = 0
   )]
-  public string Name
-  {
-    get => name;
-    set => name = value;
-  }
-  private string name = "Hello, World";
+  public string Name { get; set; } = "Hello, World";
 
   private protected sealed override void AfterEndProcessing()
   {
