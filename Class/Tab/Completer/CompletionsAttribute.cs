a8237609f86ed31bb4a13f80e5e147b1ddea6759
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-24 22:49:06 -0800

    Refactor CompletionsAttribute class structure

--- Class/Tab/Completer/CompletionsAttribute.cs
+++ Class/Tab/Completer/CompletionsAttribute.cs
@@ -12,3 +12,25 @@ public class CompletionsAttribute(
     string[] domain
   ) => domain;
 }
+
+public abstract class CompletionsAttribute<TDomain>(
+  TDomain Domain,
+  CompletionCase Casing
+) : TabCompletionsAttribute<Completer>(
+  Casing
+)
+{
+  public bool Strict { get; init; }
+
+  private protected abstract IEnumerable<string> ResolveDomain(
+    TDomain domain
+  );
+
+  public sealed override Completer Create() => new(
+    ResolveDomain(
+      Domain
+    ),
+    Casing,
+    Strict
+  );
+}
