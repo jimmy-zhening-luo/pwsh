613e7eea94c560b952f407ac3cede29ac9c39596
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-25 16:36:13 -0800

    Refactor completion method name

--- Class/Tab/Path/PathCompleter.Denormalize.cs
+++ Class/Tab/Path/PathCompleter.Denormalize.cs
@@ -2,19 +2,36 @@ namespace Module.Tab.Path;
 
 public sealed partial class PathCompleter
 {
-  private static string CompletionString(
+  private static string Canonicalize(
     string path,
-    string accumulatedSubpath,
-    bool trailingSeparator = default
-  ) => Denormalize(
-    System.IO.Path.GetFileName(
-      path
-    ),
-    accumulatedSubpath,
-    trailingSeparator
-      ? @"\"
-      : string.Empty
-  );
+    bool preserveTrailingSeparator = default
+  )
+  {
+    string normalPath = Client.FileSystem.PathString.Normalize(
+      path,
+      preserveTrailingSeparator
+    );
+
+    string homedNormalPath = normalPath.StartsWith(
+      '~'
+    )
+      ? normalPath.Length == 1
+        ? Client.Environment.Known.Folder.Home()
+        : normalPath[1] == '\\'
+          ? Client.Environment.Known.Folder.Home(
+              normalPath[2..]
+            )
+          : normalPath
+        : normalPath;
+
+    return System.IO.Path.IsPathFullyQualified(
+      homedNormalPath
+    )
+      ? homedNormalPath
+      : PowerShellHost.CurrentDirectory(
+          homedNormalPath
+        );
+  }
 
   private static string Denormalize(
     string path,
@@ -31,4 +48,18 @@ public sealed partial class PathCompleter
       '\\',
       '/'
     );
+
+  private static string CompletionString(
+    string path,
+    string accumulatedSubpath,
+    bool trailingSeparator = default
+  ) => Denormalize(
+    System.IO.Path.GetFileName(
+      path
+    ),
+    accumulatedSubpath,
+    trailingSeparator
+      ? @"\"
+      : string.Empty
+  );
 }
