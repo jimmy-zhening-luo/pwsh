286fb3dea436651556a24aaa5f75123255f530ae
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-26 02:11:27 -0800

    merge size

--- Class/Commands/Shell/Get/Attribute/Size/GetSize.cs
+++ Class/Commands/Shell/Get/Attribute/Size/GetSize.cs
@@ -19,6 +19,31 @@ using System.Linq;
 )]
 public sealed partial class GetSize : CoreCommand
 {
+  public enum DiskSizeUnit
+  {
+    B,
+    KB,
+    MB,
+    GB,
+    TB,
+    PB
+  }
+
+  private enum DiskSizeUnitAlias
+  {
+    B,
+    KB,
+    K = KB,
+    MB,
+    M = MB,
+    GB,
+    G = GB,
+    TB,
+    T = TB,
+    PB,
+    P = PB
+  }
+
   [Parameter(
     ParameterSetName = "String",
     Position = default,
