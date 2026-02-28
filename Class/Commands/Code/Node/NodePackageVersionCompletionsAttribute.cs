266a0fc756cf1b88123a923e85626870787ca568
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-28 07:38:16 -0800

    Add "prepatch" to NodePackageVersion completions

--- Class/Commands/Code/Node/NodePackageVersionCompletionsAttribute.cs
+++ Class/Commands/Code/Node/NodePackageVersionCompletionsAttribute.cs
@@ -2,5 +2,6 @@ namespace Module.Commands.Code.Node;
 
 public sealed class NodePackageVersionCompletionsAttribute() : EnumCompletionsAttribute(
   typeof(NodePackageVersion),
-  default
+  default,
+  ["prepatch"]
 );
