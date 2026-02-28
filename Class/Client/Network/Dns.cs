cf35640a45632c525772bbe1fe2d7b516e1ee7a6
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-27 19:54:55 -0800

    Refactor Resolve method to validate URI structure and improve host resolution logic

--- Class/Client/Network/Dns.cs
+++ Class/Client/Network/Dns.cs
@@ -2,9 +2,23 @@ namespace Module.Client.Network;
 
 internal static class Dns
 {
-  internal static bool Resolve(System.Uri uri) => Resolve(uri.Host);
-  internal static bool Resolve(string host)
+  internal static bool Resolve(System.Uri uri) => uri is
   {
+    IsAbsoluteUri: true,
+    Scheme: "http" or "https",
+    Host: var host
+  }
+    && Resolve(host);
+  internal static bool Resolve(
+    [System.Diagnostics.CodeAnalysis.NotNullWhen(true)]
+    string? host
+  )
+  {
+    if (string.IsNullOrWhiteSpace(host))
+    {
+      return default;
+    }
+
     try
     {
       _ = System.Net.Dns.GetHostEntry(
