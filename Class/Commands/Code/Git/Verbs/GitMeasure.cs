1a5f5b75cc48e8d8082ea7b90446656b692971de
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-28 05:02:43 -0800

    Add GitMeasure class for Git status command implementation

---
+++ Class/Commands/Code/Git/Verbs/GitMeasure.cs
@@ -0,0 +1,15 @@
+namespace Module.Commands.Code.Git.Verbs;
+
+[Cmdlet(
+  VerbsDiagnostic.Measure,
+  "GitRepository",
+  HelpUri = "https://git-scm.com/docs/git-status"
+)]
+[Alias("gg")]
+public sealed class GitMeasure() : GitCommand("status")
+{
+  private protected sealed override List<string> ParseGitArguments()
+  {
+    return [];
+  }
+}
