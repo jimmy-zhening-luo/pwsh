477abff0480a9a8e649287c56d30f1e819138d87
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-28 09:50:49 -0800

    Update version variable name in NodeUpdateVersion.cs

--- Class/Commands/Code/Node/Verbs/NodeUpdateVersion.cs
+++ Class/Commands/Code/Node/Verbs/NodeUpdateVersion.cs
@@ -75,7 +75,7 @@ public sealed class NodeUpdateVersion() : NpmCommand("version")
       }
     }
   }
-  private string version = nameof(NodePackageVersion.patch);
+  private string version = nameof(NodeVersion.patch);
 
   private protected sealed override List<string> ParseArguments()
   {
