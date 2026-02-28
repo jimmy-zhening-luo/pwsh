0976b7d4c6c86a97f1bab8fbeba0212f9fa956f5
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-28 04:31:18 -0800

    Add String utility class with EscapeSingleQuoted and UnescapeSingleQuoted methods

---
+++ Class/Client/Console/String.cs
@@ -0,0 +1,25 @@
+namespace Module.Client.Console;
+
+internal static partial class String
+{
+  internal static string EscapeSingleQuoted(string text) => text.Contains(' ')
+  ? string.Concat(
+      "'",
+      System.Management.Automation.Language.CodeGeneration.EscapeSingleQuotedStringContent(
+        text
+      ),
+      "'"
+    )
+  : text;
+
+  internal static string UnescapeSingleQuoted(string escapedText) => (
+    escapedText.Length > 1
+    && escapedText.StartsWith('\'')
+    && escapedText.EndsWith('\'')
+  )
+    ? escapedText[1..^1].Replace(
+        "''",
+        "'"
+      )
+    : escapedText;
+}
