670c545f136ab50a1c9e631ded029614b511e9ae
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-28 15:38:31 -0800

    Mark GitArgument class and its methods as obsolete in preparation for removal

--- Class/Commands/Code/Git/GitArgument.cs
+++ Class/Commands/Code/Git/GitArgument.cs
@@ -1,12 +1,15 @@
 namespace Module.Commands.Code.Git;
 
+[System.Obsolete("Removing")]
 internal static partial class GitArgument
 {
+  [System.Obsolete("Removing")]
   [System.Text.RegularExpressions.GeneratedRegex(
     @"^(?>(?=.*[*=])(?>.+)|-(?>\w|(?>-\w[-\w]*\w)))$"
   )]
   internal static partial System.Text.RegularExpressions.Regex Regex();
 
+  [System.Obsolete("Removing")]
   [System.Text.RegularExpressions.GeneratedRegex(
     @"^(?=.)(?>HEAD)?(?<branching>(?>~|\^)?)(?<step>(?>\d{0,10}))$"
   )]
