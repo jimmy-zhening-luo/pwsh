fd333fc2067401c8c2d5ac82b741b2c3f1467f65
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-28 07:32:58 -0800

    Add Exclude parameter to EnumCompletionsAttribute

--- Class/Tab/Completer/EnumCompletionsAttribute.cs
+++ Class/Tab/Completer/EnumCompletionsAttribute.cs
@@ -2,11 +2,17 @@ namespace Module.Tab.Completer;
 
 public class EnumCompletionsAttribute(
   System.Type EnumType,
-  CompletionCase Casing = CompletionCase.Lower
+  CompletionCase Casing = CompletionCase.Lower,
+  string[]? Exclude = default
 ) : CompletionsAttribute<System.Type>(
   EnumType,
   Casing
 )
 {
-  private protected sealed override IEnumerable<string> ResolveDomain(System.Type enumType) => System.Enum.GetNames(enumType);
+  private protected sealed override IEnumerable<string> ResolveDomain(System.Type enumType) => Exclude is null
+    ? System.Enum.GetNames(enumType)
+    : new HashSet<string>(
+        System.Enum.GetNames(enumType)
+      )
+        .ExceptWith(Exclude);
 }
