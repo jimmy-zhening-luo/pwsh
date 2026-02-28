929219085371ef1fe39f11b711017a3dbef66e9f
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-28 02:27:27 -0800

    Add WinGetAdd command implementation for app management

---
+++ Class/Commands/Windows/App/WinGetAdd.cs
@@ -0,0 +1,46 @@
+namespace Module.Commands.Windows.App;
+
+[Cmdlet(
+  VerbsCommon.Add,
+  "WinGetApp",
+  HelpUri = "https://learn.microsoft.com/en-us/windows/package-manager/winget/upgrade/"
+)]
+[Alias("wga")]
+[OutputType(typeof(void))]
+public sealed class WinGetAdd() : CoreCommand
+{
+  [Parameter(
+    Position = default,
+    ValueFromRemainingArguments = true,
+    DontShow = true
+  )]
+  public string[] ArgumentList { get; set; } = [];
+
+  private protected sealed override void Postprocess()
+  {
+    List<string> args = [
+      "&",
+       Client.Environment.Known.Application.WinGet
+    ];
+
+    if (ArgumentList is [])
+    {
+      args.Add("upgrade");
+    }
+    else
+    {
+      args.Add("install");
+      args.AddRange(ArgumentList);
+    }
+
+    AddScript(string.Join(' ', ArgumentList));
+
+    ProcessSteppablePipeline();
+    EndSteppablePipeline();
+
+    if (HadNativeError)
+    {
+      Throw("winget error");
+    }
+  }
+}
