1dc7dfeef501208565d16dcea28f42d8a4a80125
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-01 20:28:42 -0800

    Refactor Folder class to use System.Environment.SpecialFolder

--- Class/Client/Environment/Known/Folder.cs
+++ Class/Client/Environment/Known/Folder.cs
@@ -1,31 +1,29 @@
 namespace Module.Client.Environment.Known;
 
-using FolderName = System.Environment.SpecialFolder;
-
 internal static partial class Folder
 {
   internal static string Windows(string path = "") => Local.GetFolder(
-    FolderName.Windows,
+    System.Environment.SpecialFolder.Windows,
     path
   );
 
   internal static string ProgramFiles(string path = "") => Local.GetFolder(
-    FolderName.ProgramFiles,
+    System.Environment.SpecialFolder.ProgramFiles,
     path
   );
 
   internal static string AppData(string path = "") => Local.GetFolder(
-    FolderName.ApplicationData,
+    System.Environment.SpecialFolder.ApplicationData,
     path
   );
 
   internal static string LocalAppData(string path = "") => Local.GetFolder(
-    FolderName.LocalApplicationData,
+    System.Environment.SpecialFolder.LocalApplicationData,
     path
   );
 
   internal static string Home(string path = "") => Local.GetFolder(
-    FolderName.UserProfile,
+    System.Environment.SpecialFolder.UserProfile,
     path
   );
 }
