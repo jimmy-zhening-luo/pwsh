ffd88dcb2546fbf01baca401eb171e9939b01c14
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-02 10:46:13 -0800

    Add descriptions for CompletionCase enum values

--- Class/Tab/CompletionCase.cs
+++ Class/Tab/CompletionCase.cs
@@ -2,7 +2,12 @@ namespace Module.Tab;
 
 internal enum CompletionCase
 {
+  [System.ComponentModel.Description("Preserve the original case of the matched completion item")]
   Preserve,
+
+  [System.ComponentModel.Description("Change the matched completion item to lowercase")]
   Lower,
+
+  [System.ComponentModel.Description("Change the matched completion item to uppercase")]
   Upper,
 }
