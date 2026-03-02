b4cecf7f9ae347b0176e865fdd237abae78557ca
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-02 09:28:14 -0800

    naive encapsulation of completers

--- Class/Tab/Completer/CompletionsAttribute.cs
+++ Class/Tab/Completer/CompletionsAttribute.cs
@@ -24,4 +24,66 @@ internal abstract class CompletionsAttribute<TDomain>(
     Casing,
     Strict
   );
+
+  internal sealed class Completer(
+    IEnumerable<string> Domain,
+    CompletionCase Casing,
+    bool Strict
+  ) : TabCompleter(Casing)
+  {
+    private protected sealed override IEnumerable<string> GenerateCompletion(string wordToComplete)
+    {
+      if (wordToComplete is "")
+      {
+        foreach (var member in Domain)
+        {
+          yield return member;
+        }
+
+        yield break;
+      }
+
+      uint matches = default;
+
+      foreach (var member in Domain)
+      {
+        if (
+          member.StartsWith(
+            wordToComplete,
+            System.StringComparison.OrdinalIgnoreCase
+          )
+        )
+        {
+          ++matches;
+          yield return member;
+        }
+      }
+
+      if (
+        Strict
+        || matches is not 1
+      )
+      {
+        yield break;
+      }
+
+      foreach (var member in Domain)
+      {
+        if (
+          member.Length > wordToComplete.Length
+          && member.IndexOf(
+            wordToComplete,
+            1,
+            System.StringComparison.OrdinalIgnoreCase
+          ) > 0
+        )
+        {
+          ++matches;
+          yield return member;
+        }
+      }
+
+      yield break;
+    }
+  }
 }
