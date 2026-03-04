4c29cf0fbe48f08364e37801d01e12ec4b9513fc
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-03 21:27:27 -0800

    Refactor Administrator property in EditPath class

--- Class/Commands/Windows/Configure/EditPath.cs
+++ Class/Commands/Windows/Configure/EditPath.cs
@@ -11,19 +11,14 @@ public sealed class EditSystemPath() : CoreCommand(true)
   [Parameter(
     HelpMessage = "Launch Environment Variables control panel as administrator to edit system variables, triggering a UAC prompt if needed."
   )]
-  public SwitchParameter Administrator
-  {
-    get => administrator;
-    set => administrator = value;
-  }
-  private bool administrator;
+  public SwitchParameter Administrator { get; set; }
 
   private protected sealed override void Postprocess()
   {
     Client.Start.ShellExecute(
       "rundll32.exe",
       "sysdm.cpl,EditEnvironmentVariables",
-      administrator
+      Administrator
     );
   }
 }
