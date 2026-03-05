c4f06594d141de27aaf4f95fdb26ea0e9f2d0e81
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-04 23:29:40 -0800

    Fix variable initialization in Variable.cs

--- Class/Client/Environment/Known/Variable.cs
+++ Class/Client/Environment/Known/Variable.cs
@@ -2,6 +2,6 @@ namespace Module.Client.Environment.Known;
 
 internal static class Variable
 {
-  internal static bool InSsh => inSsh ??= Local.Get("SSH_CLIENT") is not (null or "");
-  private static bool? inSsh;
+  internal static bool InSsh => ssh ??= Local.Get("SSH_CLIENT") is not (null or "");
+  private static bool? ssh;
 }
