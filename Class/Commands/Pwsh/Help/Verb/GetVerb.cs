c13175b96badb6408c51d40d43094238f4d0e08a
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-05 12:05:08 -0800

    Add Display attribute to VerbGroup enum

--- Class/Commands/Pwsh/Help/Verb/GetVerb.cs
+++ Class/Commands/Pwsh/Help/Verb/GetVerb.cs
@@ -12,6 +12,10 @@ public sealed class GetVerb : CoreCommand
 {
   public enum VerbGroup
   {
+    [System.ComponentModel.DataAnnotations.Display(
+      Name = "Common"
+      ShortName = "c"
+    )]
     [System.ComponentModel.Description("Common verbs: Add, Clear, Get, New, Set, ..")]
     common,
 
