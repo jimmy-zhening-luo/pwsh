1c26c0697c8b6cb87b97b3d9b2bc32fbfb218535
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-04 23:20:00 -0800

    Refactor folder methods to use aliases for SpecialFolder

--- Class/Client/Environment/Known/Folder.cs
+++ Class/Client/Environment/Known/Folder.cs
@@ -1,29 +1,36 @@
 namespace Module.Client.Environment.Known;
 
+using SpecialFolder = System.Environment.SpecialFolder;
+
 internal static partial class Folder
 {
-  internal static string Windows(string path = "") => Local.GetFolder(
-    System.Environment.SpecialFolder.Windows,
+  internal static string Windows() => Local.GetFolder(SpecialFolder.Windows);
+  internal static string Windows(string path) => Local.GetFolder(
+    SpecialFolder.Windows,
     path
   );
 
-  internal static string ProgramFiles(string path = "") => Local.GetFolder(
-    System.Environment.SpecialFolder.ProgramFiles,
+  internal static string ProgramFiles() => Local.GetFolder(SpecialFolder.ProgramFiles);
+  internal static string ProgramFiles(string path) => Local.GetFolder(
+    SpecialFolder.ProgramFiles,
     path
   );
 
-  internal static string AppData(string path = "") => Local.GetFolder(
-    System.Environment.SpecialFolder.ApplicationData,
+  internal static string AppData() => Local.GetFolder(SpecialFolder.ApplicationData);
+  internal static string AppData(string path) => Local.GetFolder(
+    SpecialFolder.ApplicationData,
     path
   );
 
-  internal static string LocalAppData(string path = "") => Local.GetFolder(
-    System.Environment.SpecialFolder.LocalApplicationData,
+  internal static string LocalAppData() => Local.GetFolder(SpecialFolder.LocalApplicationData);
+  internal static string LocalAppData(string path) => Local.GetFolder(
+    SpecialFolder.LocalApplicationData,
     path
   );
 
-  internal static string Home(string path = "") => Local.GetFolder(
-    System.Environment.SpecialFolder.UserProfile,
+  internal static string Home() => Local.GetFolder(SpecialFolder.UserProfile);
+  internal static string Home(string path) => Local.GetFolder(
+    SpecialFolder.UserProfile,
     path
   );
 }
