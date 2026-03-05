acfe265c083be5d42fb3e7038325bee107217d1f
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-04 23:28:26 -0800

    Add Code method to Folder class for path handling

--- Class/Client/Environment/Known/Folder.Relative.cs
+++ Class/Client/Environment/Known/Folder.Relative.cs
@@ -2,13 +2,17 @@ namespace Module.Client.Environment.Known;
 
 internal static partial class Folder
 {
-  internal static string SystemDrive(string path = "") => File.PathString.FullPathLocationRelative(
-    Windows(".."),
+  internal static string SystemDrive(string path) => File.PathString.FullPathLocationRelative(
+    SystemDrive(),
     path
   );
+  internal static string SystemDrive() => system ??= Windows("..");
+  private static string? system;
 
-  internal static string Code(string path = "") => File.PathString.FullPathLocationRelative(
-    Home("code"),
+  internal static string Code(string path) => File.PathString.FullPathLocationRelative(
+    Code(),
     path
   );
+  internal static string Code() => code ??= Home("code");
+  private static string? code;
 }
