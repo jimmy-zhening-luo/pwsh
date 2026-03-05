dc3e78e1bbfd6a8470d980c726b65f788057f5f4
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-05 14:10:39 -0800

    Implement PowerShellHost class with IDisposable pattern for resource management

--- Class/Commands/Command.PowerShellHost.cs
+++ Class/Commands/Command.PowerShellHost.cs
@@ -1,4 +1,60 @@
 namespace Module.Commands;
 
 public abstract partial class CoreCommand
-{ }
+{
+  private class PowerShellHost : System.IDisposable
+  {
+    private PowerShell? powershell;
+    private SteppablePipeline? steppablePipeline;
+
+    internal PowerShellHost()
+    {
+      powershell = Module.Create();
+    }
+
+    ~PowerShellHost()
+    {
+      Dispose(default);
+    }
+
+    [System.Diagnostics.CodeAnalysis.MemberNotNullWhen(
+      default,
+      nameof(powershell)
+    )]
+    private bool Disposed { get; set; }
+
+    public void Dispose()
+    {
+      Dispose(true);
+
+      System.GC.SuppressFinalize(this);
+    }
+
+    private void CleanPipeline()
+    {
+      if (steppablePipeline is not null)
+      {
+        _ = steppablePipeline.End();
+        steppablePipeline.Clean();
+        steppablePipeline.Dispose();
+        steppablePipeline = default;
+      }
+    }
+
+    private void Dispose(bool disposing)
+    {
+      if (!Disposed)
+      {
+        if (disposing)
+        {
+          CleanPipeline();
+
+          powershell.Dispose();
+          powershell = default;
+        }
+
+        Disposed = true;
+      }
+    }
+  }
+}
