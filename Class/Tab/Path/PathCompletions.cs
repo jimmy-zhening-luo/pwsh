95c279408cba7e7bd7721168f04fea6e4e1fd2f0
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-06 13:52:54 -0800

    Refactor attribute checks for clarity and consistency

--- Class/Tab/Path/PathCompletions.cs
+++ Class/Tab/Path/PathCompletions.cs
@@ -319,8 +319,8 @@ internal class PathCompletionsAttribute(
       foreach (var item in items)
       {
         if (
-          item.Attributes & System.IO.FileAttributes.Hidden == System.IO.FileAttributes.None
-          || item.Attributes & System.IO.FileAttributes.System == System.IO.FileAttributes.None
+          (item.Attributes & System.IO.FileAttributes.Hidden) == default
+          || (item.Attributes & System.IO.FileAttributes.System) == default
         )
         {
           ++Index;
