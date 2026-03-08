0a0605c0cf88eec81f91a9439880133738fe1061
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-08 06:46:37 -0700

    Add initial implementation of Edit handler class

---
+++ Class/Client/File/Edit.cs
@@ -0,0 +1,60 @@
+namespace Module.Client.File;
+
+static internal class Handler
+{
+  static void Edit() => Edit(string.Empty);
+  static void Edit(
+    string path,
+    IList<string> arguments = []
+  ) => Start.CreateProcess(
+    Environment.Known.Application.VSCode,
+    [
+      path,
+      .. arguments,
+    ]
+  );
+  static void Edit(
+    string path,
+    string profile,
+    IList<string> arguments = []
+  ) => Edit(
+    path,
+    [
+      $"--profile={profile}",
+      .. arguments,
+    ]
+  );
+  static void Edit(
+    string path,
+    bool newWindow,
+    bool reuseWindow,
+    IList<string> arguments = []
+  ) => Edit(
+    path,
+    [
+      .. (
+        newWindow
+          ? ["--new-window"]
+          : reuseWindow
+            ? ["--reuse-window"]
+            : []
+      ),
+      .. arguments,
+    ]
+  );
+  static void Edit(
+    string path,
+    string profile,
+    bool newWindow,
+    bool reuseWindow,
+    IList<string> arguments = []
+  ) => Edit(
+    path,
+    newWindow,
+    reuseWindow,
+    [
+      $"--profile={profile}",
+      .. arguments,
+    ]
+  );
+}
