a1e09c7fa40eaf0bc9b4b0c6e9cff0fd0b4fc28a
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-08 07:31:51 -0700

    Refactor VirtualStartWorkspace class structure

--- Class/Commands/Shell/Start/Workspace/VirtualStartWorkspace.cs
+++ Class/Commands/Shell/Start/Workspace/VirtualStartWorkspace.cs
@@ -1,7 +1,16 @@
 namespace Module.Commands.Shell.Start.Workspace;
 
-abstract public class VirtualStartWorkspace() : NativeCommand(true, true)
+abstract public class VirtualStartWorkspace() : CoreCommand(true)
 {
+  private enum VSCodeProfile
+  {
+    Default,
+    Setting,
+    Svelte,
+    Python,
+    C,
+  }
+
   private const string FlagNewWindow = "--new-window";
   private const string FlagReuseWindow = "--reuse-window";
 
@@ -10,11 +19,55 @@ abstract public class VirtualStartWorkspace() : NativeCommand(true, true)
 
   [Parameter(Position = 1)]
   [ValidateNotNullOrWhiteSpace]
+  [EnumCompletions(typeof(VSCodeProfile))]
   public string Name
+  {
+    set
+    {
+      List<string> profileNames = [
+        .. Enum.GetNames<VSCodeProfile>(),
+      ];
+
+      var profileName = profileNames.Find(
+        pn => pn.StartsWith(
+          value,
+          System.StringComparison.OrdinalIgnoreCase
+        )
+      );
+
+      if (profileName is not null)
+      {
+        profile = profileName switch
+        {
+          "Default" => string.Empty,
+          "C" => "C++",
+          var pn => pn,
+        };
+
+        return;
+      }
+
+      ArgumentList = [
+        value,
+        .. ArgumentList,
+      ];
+    }
+  }
+  private string profile = string.Empty;
+
+  [Parameter(
+    Position = 2,
+    ValueFromRemainingArguments = true,
+    DontShow = true,
+    HelpMessage = "Additional arguments"
+  )]
+  [ValidateLength(1, int.MaxValue)]
+  [Tab.PathCompletions]
+  public string[] ArgumentList
   {
     private get;
     set;
-  } = string.Empty;
+  } = [];
 
   [Parameter]
   public SwitchParameter Window
@@ -30,49 +83,44 @@ abstract public class VirtualStartWorkspace() : NativeCommand(true, true)
     set;
   }
 
-  sealed override private protected string CommandPath { get; } = Client.Environment.Known.Application.VSCode;
-
-  sealed override private protected void PreprocessArguments()
+  sealed override private protected void Preprocess()
   {
-    switch (Name)
-    {
-      case "":
-        break;
-
-      case "se":
-        NativeArguments.Add("--profile=Setting");
-        break;
-
-      case var profile when IsNativeArgument(profile):
-        NativeArguments.Add(Name);
-        break;
-
-      default:
-        WriteWarning("Profiles not supported except for se (Setting)");
-        break;
-    }
-
     if (Window || ReuseWindow)
     {
-      _ = NativeArguments.RemoveAll(a => a is FlagNewWindow);
-      _ = NativeArguments.RemoveAll(a => a is FlagReuseWindow);
-
-      NativeArguments.Add(
-        Window
-          ? FlagNewWindow
-          : FlagReuseWindow
-      );
+      _ = ArgumentList.RemoveAll(a => a is FlagNewWindow);
+      _ = ArgumentList.RemoveAll(a => a is FlagReuseWindow);
     }
   }
 
-  sealed override private protected List<string> BuildNativeCommand() => [
-    InCurrentLocation
-    && path is ""
-    && (
-      Pwd() == Client.Environment.Known.Folder.Code()
-      || Pwd() == Client.Environment.Known.Folder.Home()
-    )
-      ? string.Empty
-      : ReanchorPath(path),
-  ];
+  sealed override private protected void Process()
+  {
+    path = InCurrentLocation
+      && path is ""
+      && (
+        Pwd() == Client.Environment.Known.Folder.Code()
+        || Pwd() == Client.Environment.Known.Folder.Home()
+      )
+        ? string.Empty
+        : ReanchorPath(path);
+
+    if (name is "")
+    {
+      Client.File.Handler.Edit(
+        path,
+        newWindow,
+        reuseWindow,
+        ArgumentList
+      );
+    }
+    else
+    {
+      Client.File.Handler.Edit(
+        path,
+        profile,
+        newWindow,
+        reuseWindow,
+        ArgumentList
+      );
+    }
+  }
 }
