3323abbaec6d9e0acef7d4dc52695b6b936c482c
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-09 02:35:18 -0700

    Reorder method calls in PreprocessArguments for improved clarity and consistency

--- Class/Commands/Code/RemoteNativeVerbCommand.cs
+++ Class/Commands/Code/RemoteNativeVerbCommand.cs
@@ -57,8 +57,8 @@ abstract public class RemoteNativeVerbCommand(
       WorkingDirectory = string.Empty;
     }
 
-    PreprocessOtherArguments();
     PreprocessIntrinsicVerb();
     PreprocessWorkingDirectory();
+    PreprocessOtherArguments();
   }
 }
