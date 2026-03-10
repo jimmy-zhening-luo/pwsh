f56db9eeffe9895ab8ad0936be403979d26b5875
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-09 20:36:24 -0700

    Format properties in TCompleter for improved readability

--- Class/Tab/Completers/Intrinsics/TCompleter.cs
+++ Class/Tab/Completers/Intrinsics/TCompleter.cs
@@ -5,8 +5,10 @@ abstract internal class TCompleter(
   CompletionResultType CompletionType
 ) : ICompleter
 {
-  public CompletionCase Case { get; init; } = Case;
-  public CompletionResultType CompletionType { get; init; } = CompletionType;
+  public CompletionCase Case
+  { get; init; } = Case;
+  public CompletionResultType CompletionType
+  { get; init; } = CompletionType;
 
   abstract private protected IEnumerable<CompletionResultRecord> GenerateCompletion(string wordToComplete);
   IEnumerable<CompletionResultRecord> ICompleter.GenerateCompletion(string wordToComplete) => GenerateCompletion(wordToComplete);
