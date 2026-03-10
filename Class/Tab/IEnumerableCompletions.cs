66d044c94c2e5351ef93f950045ed246e7be13b6
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-09 21:10:17 -0700

    Remove unused constructor parameter in IEnumerableCompletions

--- Class/Tab/IEnumerableCompletions.cs
+++ Class/Tab/IEnumerableCompletions.cs
@@ -3,5 +3,5 @@ namespace PowerModule.Tab.Generic;
 abstract internal class IEnumerableCompletionsAttribute(
   IEnumerable<string> Domain) : CompletionsAttribute<IEnumerable<string>>(Domain)
 {
-  sealed override private protected IEnumerable<string> EnumerateDomain(IEnumerable<string> Domain) => domain;
+  sealed override private protected IEnumerable<string> EnumerateDomain(IEnumerable<string> Domain) => Domain;
 }
