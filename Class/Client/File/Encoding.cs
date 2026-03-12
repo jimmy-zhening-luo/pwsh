9903c7d36b3849b6bf3495a3fc7caaf0366441c9
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-11 23:43:48 -0700

    Remove unnecessary descriptions from Encoding enum

--- Class/Client/File/Encoding.cs
+++ Class/Client/File/Encoding.cs
@@ -2,58 +2,15 @@ namespace PowerModule.Client.File;
 
 enum Encoding
 {
-  [System.ComponentModel.Description(
-    "The encoding for the current culture's ANSI code page"
-  )]
   ansi,
-
-  [System.ComponentModel.Description(
-    "UTF-8 format (no BOM)"
-  )]
   utf8,
-
-  [System.ComponentModel.Description(
-    "ASCII (7-bit) character set"
-  )]
   ascii,
-
-  [System.ComponentModel.Description(
-    "UTF-16 (big-endian)"
-  )]
   bigendianunicode,
-
-  [System.ComponentModel.Description(
-    "UTF-32 (big-endian)"
-  )]
   bigendianutf32,
-
-  [System.ComponentModel.Description(
-    "The default encoding for MS-DOS and console programs"
-  )]
   oem,
-
-  [System.ComponentModel.Description(
-    "UTF-16 (little-endian)"
-  )]
   unicode,
-
-  [System.ComponentModel.Description(
-    "UTF-7"
-  )]
   utf7,
-
-  [System.ComponentModel.Description(
-    "UTF-8 with Byte Order Mark (BOM)"
-  )]
   utf8bom,
-
-  [System.ComponentModel.Description(
-    "UTF-8 without Byte Order Mark (no BOM)"
-  )]
   utf8nobom,
-
-  [System.ComponentModel.Description(
-    "UTF-32 (little-endian)"
-  )]
   utf32,
 }
