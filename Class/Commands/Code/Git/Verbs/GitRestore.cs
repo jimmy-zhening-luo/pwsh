ea1d784d4dddbed75685230000403b1b6e474ea5
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-11 19:32:16 -0700

    Refactor argument declaration in GitRestore.cs

--- Class/Commands/Code/Git/Verbs/GitRestore.cs
+++ Class/Commands/Code/Git/Verbs/GitRestore.cs
@@ -20,7 +20,7 @@ sealed public class GitRestore() : GitCommand("pull")
       WorkingDirectory = string.Empty;
     }
 
-    string[] arguments = new [5];
+    var arguments = new string[5];
     Arguments.CopyTo(arguments, 0);
     Arguments.Clear();
 
