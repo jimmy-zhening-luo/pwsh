534013d7bf1d7f4b97dfae85d4f4d3f0585315d9
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-19 15:52:08 -0700

    Remove GetPowerShellHostLocation method from StandardModule

--- Class/StandardModule.cs
+++ Class/StandardModule.cs
@@ -4,26 +4,4 @@ static class StandardModule
 {
   internal const string Management = "Microsoft.PowerShell.Management";
   internal const string Utility = "Microsoft.PowerShell.Utility";
-
-  static internal string GetPowerShellHostLocation()
-  {
-    using var ps = PowerShell.Create(
-      RunspaceMode.CurrentRunspace
-    );
-
-    var currentLocation = ps
-      .AddCommand(
-        $@"{Management}\Get-Location"
-      )
-      .Invoke()[default]
-      .BaseObject
-      .ToString();
-
-    System.ArgumentException.ThrowIfNullOrEmpty(
-      currentLocation,
-      "PowerShell host current location"
-    );
-
-    return currentLocation;
-  }
 }
