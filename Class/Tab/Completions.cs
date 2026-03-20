3f3196f98e7ecbf4622cf889c5c8e6b2eecc030f
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-20 02:30:43 -0700

    Remove redundant CompletionsAttribute class and simplify domain handling

--- Class/Tab/Completions.cs
+++ Class/Tab/Completions.cs
@@ -1,10 +1,5 @@
 namespace PowerModule.Tab;
 
-sealed class CompletionsAttribute(params string[] Domain) : CompletionsAttribute<string[]>(Domain)
-{
-  public string[] Domain
-  { get; } = Domain;
-}
 abstract class CompletionsAttribute<TCollection>(TCollection Domain) : Factory.SetCompleterFactory<TCollection>(Domain) where TCollection : ICollection<string>
 {
   sealed override private protected ICollection<string> EvaluateDomain(TCollection Domain) => Domain;
